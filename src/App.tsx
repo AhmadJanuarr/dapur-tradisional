0a3827a6ffa048e19a6ca8bec99ce61ac7e7006c
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-12 22:35:43 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/Home"
+import NotFoundPage from "./pages/404"
+import AppLayout from "./components/Layout/App"
+import Authentication from "./pages/auth/Authentication"
+import Recipes from "./pages/Recipes"
 
 export default function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  );
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          {/* Halaman Authentication (Login & Signup) */}
+          <Route path="/login" element={<Authentication />} />
+          <Route path="/signup" element={<Authentication />} />
+
+          <Route path="/recipes" element={<Recipes />} />
+          {/* Halaman 404 */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  )
 }
