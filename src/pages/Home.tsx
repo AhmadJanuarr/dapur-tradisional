d4c9228a9f8131a26e329230057a96078725f659
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-12 22:37:58 +0700

    feat: create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import HeroSection from "../components/Views/Home/Hero"
+
+export default function HomePage() {
+  return (
+    <React.Fragment>
+      <HeroSection
+        heading="Masak Dengan Lebih Mudah & Kreatif"
+        description="Dengan ResepHub, Anda bisa menemukan inspirasi untuk masakan lezat setiap hari. Cari resep favorit Anda sekarang!"
+        callToAction="Jelajahi Resep Terbaru"
+        src="/svg/hero.svg"
+        to="/recipes"
+      />
+    </React.Fragment>
+  )
+}
