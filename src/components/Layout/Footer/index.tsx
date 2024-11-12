60babc22bc9724514a852c027c494b8dd133b58e
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-12 23:14:18 +0700

    feat: create src/components/Layout/Footer/index.tsx

---
+++ src/components/Layout/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full justify-center py-5">
+      <p>© 2024 Your Company Name. All rights reserved. </p>
+    </footer>
+  )
+}
