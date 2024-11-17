78a8ec2ab9c7d28462684ba2ea6b6257987455d5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-17 10:20:57 +0700

    refactor code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,18 @@ export default {
         inter: ["Inter", "sans-serif"],
       },
       screens: {
-        sm: { min: "640px" },
-        md: { min: "768px" },
-        lg: { min: "1280px" },
-        xl: { min: "1536px" },
+        sm: {
+          min: "640px",
+        },
+        md: {
+          min: "768px",
+        },
+        lg: {
+          min: "1280px",
+        },
+        xl: {
+          min: "1536px",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -58,6 +66,16 @@ export default {
           3: "hsl(var(--chart-3))",
           4: "hsl(var(--chart-4))",
           5: "hsl(var(--chart-5))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
         },
       },
     },
