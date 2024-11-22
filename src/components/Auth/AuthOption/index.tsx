52d09ffe5581d5633cef476f22fbcd7d5707e792
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-22 23:00:35 +0700

    refactor code

--- src/components/Auth/AuthOption/index.tsx
+++ src/components/Auth/AuthOption/index.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom"
 import { Github, Mail } from "lucide-react"
-import { SosialLoginButton } from "../../SosialLogin"
-import { Divider } from "../../Divide/Divider"
+import { SocialLoginButton } from "@/components/SocialLogin"
+import { Divider } from "@/components/Divide/Divider"
 
 export function AuthOption({ linkText, linkTo }: { linkText: string; linkTo: string }) {
   return (
     <div className="space-y-4">
       <Divider text="Atau masuk dengan" />
-      <SosialLoginButton provider="Google" icon={<Mail />} />
-      <SosialLoginButton provider="Github" icon={<Github />} />
+      <SocialLoginButton provider="Google" icon={<Mail />} />
+      <SocialLoginButton provider="Github" icon={<Github />} />
 
       <p className="text-center text-[0.8rem]">
         {linkText}{" "}
-        <Link to={`/${linkTo}`} className="text-blue-500">
+        <Link to={`/auth/${linkTo}`} className="text-blue-500">
           {linkTo}
         </Link>
       </p>
