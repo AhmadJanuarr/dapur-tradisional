7c40ac4306e80e939ba557bd0bce21492f288394
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-11-22 22:27:20 +0700

    add schema recipe

--- src/schemas/FormSchema.ts
+++ src/schemas/FormSchema.ts
@@ -10,3 +10,12 @@ export const formSchemaLogin = z.object({
     email: z.string().email(),
     password: z.string().min(8),
 })
+
+export const formSchemaRecipe = z.object({
+    title: z.string().max(80),
+    description: z.string().max(500),
+    image: z.string(),
+    category: z.string(),
+    ingredients: z.array(z.string().max(100)),
+    steps: z.array(z.string().max(500)),
+})
