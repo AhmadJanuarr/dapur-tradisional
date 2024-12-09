136a8d9ea21522bfddc1760647f49f1d8c36f3a5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-12-09 21:05:42 +0700

    refactor code

--- src/components/Admin/RecipeList.tsx
+++ src/components/Admin/RecipeList.tsx
@@ -1,4 +1,6 @@
 import { TableBody, TableCell, TableRow } from "@/components/ui/table"
+import { EditIcon, Trash2 } from "lucide-react"
+import { Button } from "../ui/button"
 
 interface Recipe {
   title: string
@@ -17,23 +19,39 @@ export default function RecipeList({ recipes }: { recipes: Recipe[] }) {
           <TableCell className="font-medium ">{id}</TableCell>
           <TableCell>{title}</TableCell>
           <TableCell>
-            <img src={`${import.meta.env.VITE_API_URL}/images/${image}`} width={100} height={100} alt="image" />
+            <img
+              src={`${import.meta.env.VITE_API_URL}/images/${image}`}
+              width={100}
+              height={100}
+              alt="image"
+              loading="lazy"
+            />
           </TableCell>
           <TableCell>{description}</TableCell>
           <TableCell>{category.replace("_", " ")}</TableCell>
           <TableCell>
-            <ol className="list-disc list-inside">
+            <ol className="list-inside list-disc">
               {ingredients.map(({ name, id }: { name: string; id: number }) => (
                 <li key={id}>{name}</li>
               ))}
             </ol>
           </TableCell>
           <TableCell>
-            <ol className="list-disc list-inside">
+            <ol className="list-inside list-disc">
               {steps.map(({ name, id }: { name: string; id: number }) => (
                 <li key={id}>{name}</li>
               ))}
             </ol>
+          </TableCell>
+          <TableCell>
+            <div className="flex justify-center gap-2">
+              <Button className="btn btn-primary btn-sm" variant="default">
+                <EditIcon />
+              </Button>
+              <Button className="btn btn-error btn-sm" variant="destructive">
+                <Trash2 />
+              </Button>
+            </div>
           </TableCell>
         </TableRow>
       ))}
