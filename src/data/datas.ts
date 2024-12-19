f072228d844f6992790d35aad2a35f94e7b95b09
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-12-19 15:41:34 +0700

    add menu data structure for navigation links

---
+++ src/data/datas.ts
@@ -0,0 +1,20 @@
+export const LIST_MENU = {
+  usefulLinks: [
+    { name: "Beranda", href: "/" },
+    { name: "Resep", href: "/" },
+    { name: "Tentang", href: "/" },
+    { name: "Kontak", href: "/" },
+  ],
+  resources: [
+    { name: "Blog", href: "/blog" },
+    { name: "Panduan", href: "/guides" },
+    { name: "API Dokumentasi", href: "/api-docs" },
+    { name: "Karir", href: "/careers" },
+  ],
+  support: [
+    { name: "FAQ", href: "/faq" },
+    { name: "Bantuan", href: "/help" },
+    { name: "Ketentuan Layanan", href: "/terms" },
+    { name: "Kebijakan Privasi", href: "/privacy" },
+  ],
+};
