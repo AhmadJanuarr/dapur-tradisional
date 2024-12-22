79bc25855d2c1dcafd35d2449770756f9d2f80fc
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-12-22 15:57:04 +0700

    feat: update HeroSection component with new layout, improved styling, and search functionality

--- src/components/Views/Recipes/HeroSection.tsx
+++ src/components/Views/Recipes/HeroSection.tsx
@@ -1,28 +1,33 @@
-import { Input } from "../../ui/input"
+import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
 
 export default function HeroSection() {
   return (
-    <div className="relative h-[700px] w-full bg-cover bg-center" style={{ backgroundImage: "url('/img/hero.png')" }}>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative flex h-full items-center justify-center text-white">
-        <div className="p-5 text-center">
-          <h1 className="text-5xl font-bold">Resep Makanan Ringan Khas Daerah</h1>
-          <p className="mt-4 text-lg">
-            Jelajahi kekayaan kuliner Indonesia melalui berbagai makanan ringan khas daerah yang penuh cita rasa dan
-            keunikan.
+    <div className="mt-20 flex w-full border">
+      <div className="flex w-full rounded-xl bg-[#F9802D]">
+        <div className="flex w-1/2 flex-col gap-2 pl-4 md:py-10 lg:py-32 lg:pl-8">
+          <h1 className="text-xl font-semibold text-white md:text-2xl xl:text-4xl">
+            Dapur tradisional - resep makanan khas daerah
+          </h1>
+          <p className="text-primary text-white">
+            Temukan resep makanan khas daerah Indonesia yang lezat dan mudah dibuat
           </p>
-          <div className="relative mx-auto md:w-1/2">
-            <div className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 transform">
+          <div className="relative ">
+            <div className="pointer-events-none absolute left-3 top-1/2 -translate-y-1 transform">
               <Search className="text-black" size={24} />
             </div>
             <Input
               type="search"
               placeholder="Cari resep..."
-              className="mt-4 rounded-xl bg-white pl-12 placeholder:text-black"
+              className="text- black mt-4 rounded-md border-none bg-white pl-12 placeholder:text-black"
+              sonner-spinner
               aria-label="Search recipes"
             />
           </div>
+        </div>
+
+        <div className="flex w-1/2 justify-center">
+          <img src="/svg/cooking.svg" alt="cooking image" className="w-[200px] lg:w-[400px]" />
         </div>
       </div>
     </div>
