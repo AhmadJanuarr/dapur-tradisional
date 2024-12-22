014a696a830c47c4aa7934ea059fec39678c7b2b
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-12-22 15:57:29 +0700

    feat: update HeadingSection component to include a button for viewing all items

--- src/components/Views/Recipes/HeadingSection.tsx
+++ src/components/Views/Recipes/HeadingSection.tsx
@@ -1,14 +1,27 @@
-export default function HeadingSection({ heading, description }: { heading: string; description: string }) {
-  return (
-    <section className="w-full pb-10 pt-24">
-      <div className="w-full">
-        <h1 className="text-5xl font-bold">{heading}</h1>
-        <p className="mt-2">{description}</p>
-        {/* {filter && (
-            <div className="flex w-full items-center justify-center">
+import { Button } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
 
-            </div>
-        )} */}
+export default function HeadingSection({
+  heading,
+  description,
+  isShowButton,
+}: {
+  heading: string
+  description: string
+  isShowButton: boolean
+}) {
+  return (
+    <section className="w-full pt-24 pb-10">
+      <div className="flex items-center justify-between w-full">
+        <div>
+          <h1 className="text-5xl font-bold">{heading}</h1>
+          <p className="mt-2">{description}</p>
+        </div>
+        {isShowButton && (
+          <Button variant="link" className="text-[16px]">
+            Lihat Semua <ArrowRight />
+          </Button>
+        )}
       </div>
     </section>
   )
