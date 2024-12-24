04711b5ec48d2d9ac0de2990117fa1476a2f1515
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-12-24 22:18:43 +0700

    feat: improve layout and styling of SupportFaq component for better readability

--- src/components/Views/Home/SupportFaq.tsx
+++ src/components/Views/Home/SupportFaq.tsx
@@ -9,21 +9,21 @@ interface SupportFaqProps {
 
 export default function SupportFaq() {
   return (
-    <section className="flex flex-col items-center justify-center w-full py-32 mt-20">
+    <section className="mt-20 flex w-full flex-col items-center justify-center py-32">
       <div className="w-full">
         <h1 className="pb-5 text-4xl text-gray-900">Frequently Asked Questions</h1>
         <p className="text-gray-500">Jawaban untuk Pertanyaan Umum tentang Resep dan Penggunaan Website</p>
       </div>
 
-      <div className="grid justify-center w-full grid-cols-1 py-10 place-items-center justify-items-center md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid w-full grid-cols-1 place-items-center justify-center justify-items-center py-10 md:grid-cols-2 xl:grid-cols-3">
         {SupportsFaqList.map(({ question, answer, icon: IconComponent }: SupportFaqProps, index) => (
           <div className="flex gap-5 py-5 pl-5" key={index}>
-            <div className="flex items-center justify-center w-12 h-12 p-2 border rounded-sm">
+            <div className="flex h-12 w-12 items-center justify-center rounded-sm border p-2">
               <IconComponent />
             </div>
             <div className="flex flex-col">
               <h2 className="text-lg font-semibold text-gray-800">{question}</h2>
-              <p className="mt-2 text-sm text-gray-600">{answer}</p>
+              <p className="text-lopsm mt-2 text-gray-600">{answer}</p>
             </div>
           </div>
         ))}
