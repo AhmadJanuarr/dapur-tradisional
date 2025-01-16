d37aafab007e6f64d75b21ced462647ea77137b5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-16 23:31:28 +0700

    feat: update form validation schemas with custom error messages

--- src/schemas/FormSchema.ts
+++ src/schemas/FormSchema.ts
@@ -1,14 +1,26 @@
 import { z } from "zod";
 
-export const formSchemaSignup = z.object({
-    name: z.string().min(6).max(50),
-    email: z.string().email(),
-    password: z.string().min(8),
+export const formSchemaRegister = z.object({
+    name: z.string().min(6, {
+        message : "Nama minimal 6 karakter",
+    }).max(50,{
+        message : "Nama maksimal 50 karakter",
+    }),
+    email: z.string().min(1, {
+        message : "Email harus tidak boleh kosong",
+    }).email(),
+    password: z.string().min(8,{
+        message : "Password minimal 8 karakter",
+    }),
 })
 
 export const formSchemaLogin = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
+    email: z.string().min(1,{
+        message : "Email harus tidak boleh kosong",
+    }).email(),
+    password: z.string().min(8, {
+        message : "Password minimal 8 karakter",
+    }),
 })
 
 export const formSchemaRecipe = z.object({
