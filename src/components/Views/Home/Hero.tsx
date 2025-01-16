df92cfd88ec567c1b3c1df92d3784420d5bb9173
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-16 23:55:55 +0700

    feat: update HeroSection component with enhanced layout and additional link functionality

--- src/components/Views/Home/Hero.tsx
+++ src/components/Views/Home/Hero.tsx
@@ -2,34 +2,37 @@ import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button"
 import { Dessert } from "lucide-react"
 
-export default function HeroSection({
-  heading,
-  description,
-  callToAction,
-  src,
-  to,
-}: {
+type HeroSectionProps = {
   heading: string
   description: string
   callToAction: string
-  src: string
   to: string
-}) {
+  href: string
+}
+export default function HeroSection({ heading, description, callToAction, to, href }: HeroSectionProps) {
   return (
-    <section className="w-full py-10 mx-auto mt-20">
-      <div className="flex items-center">
-        <div className="w-full md:w-1/2">
-          <h1 className="text-5xl leading-tight">{heading}</h1>
-          <p className="mt-6">{description}</p>
-          <Link to={to}>
-            <Button className="mt-5">
-              <Dessert />
-              {callToAction}
-            </Button>
-          </Link>
-        </div>
-        <div className="justify-center hidden w-1/2 md:flex">
-          <img src={src} alt={src} />
+    <section
+      className="flex justify-center w-full h-screen bg-center bg-no-repeat bg-cover"
+      style={{ backgroundImage: 'url("/img/hero.jpg")' }}
+    >
+      <div className="absolute z-20 w-full h-screen bg-black bg-opacity-70"></div>
+      <div className="z-30 flex items-center justify-center">
+        <div className="w-3/4 text-center text-white">
+          <h1 className="font-italian text-[2rem] font-normal lg:text-[5rem]">{heading}</h1>
+          <p className="mt-6 text-[0.8rem] lg:text-[1rem]">{description}</p>
+          <div className="flex justify-center gap-5 mt-10">
+            <Link to={to}>
+              <Button className="border bg-white text-[0.7rem] text-black hover:border-white hover:text-white lg:py-5 lg:text-[1.rem]">
+                <Dessert />
+                {callToAction}
+              </Button>
+            </Link>
+            <a href={href} target="_blank">
+              <Button variant="link" className="border text-[0.7rem] text-white lg:py-5 lg:text-[1rem]">
+                Dokumentasi
+              </Button>
+            </a>
+          </div>
         </div>
       </div>
     </section>
