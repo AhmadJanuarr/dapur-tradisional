9cce042afa545244b4320b3a37250f8e587434e9
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-16 23:34:23 +0700

    feat: enhance TraditionalCuisineBenefits component with improved styling and layout

--- src/components/Views/Home/TraditionalCuisineBenefits.tsx
+++ src/components/Views/Home/TraditionalCuisineBenefits.tsx
@@ -9,10 +9,9 @@ interface TraditionalProps {
 
 export default function TraditionalCuisineBenefits() {
   return (
-    <section className="flex flex-col items-center justify-center w-full py-32 mt-20">
-      <div className="absolute -z-10 mt-32 h-[800px] w-full bg-[#F5F5F4]"></div>
-      <h1 className="w-1/2 py-10 text-4xl text-center text-gray-500 font-regular ">
-        Mengapa <span className="text-gray-900 ">Masakan Tradisional</span> Tak Tergantikan?
+    <section className="flex flex-col items-center justify-center w-4/5 py-20 mx-auto mt-10">
+      <h1 className="w-1/2 py-10 text-5xl leading-tight text-center font-domine">
+        Mengapa Masakan Tradisional Tak Tergantikan?
       </h1>
       <div className="grid grid-cols-1 overflow-hidden bg-white border shadow-lg rounded-2xl md:grid-cols-3 ">
         {ListTraditionalCuisineBenefits.map(({ title, description, icon: IconComponent }: TraditionalProps, index) => (
@@ -23,11 +22,11 @@ export default function TraditionalCuisineBenefits() {
             } ${index % 3 !== 2 ? "md:border-r" : ""}`}
           >
             <div className="flex items-center justify-center w-16 h-16 shadow-xl rounded-xl">
-              <IconComponent className="h-8 w-8 text-[#FFA500]" />
+              <IconComponent className="w-8 h-8 text-black" />
             </div>
 
-            <h2 className="my-6 text-lg font-semibold text-gray-800">{title}</h2>
-            <p className="mt-2 text-sm text-gray-600">{description}</p>
+            <h2 className="my-6 text-lg font-semibold text-black">{title}</h2>
+            <p className="mt-2 text-sm text-black">{description}</p>
           </div>
         ))}
       </div>
