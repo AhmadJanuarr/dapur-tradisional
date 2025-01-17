f61c88cd680c7a5d8b6bf0c04725051b8add59db
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-17 08:26:55 +0700

    feat: refactor RecipeStatistics component to accept props for dynamic content

--- src/components/Views/Home/RecipeStatistics.tsx
+++ src/components/Views/Home/RecipeStatistics.tsx
@@ -1,10 +1,15 @@
 import { BookOpenText, Database } from "lucide-react"
 
-export default function RecipeStatistics() {
+interface StatisticsProps {
+  heading: string
+  img: string
+  description: string
+}
+export default function RecipeStatistics({ heading, img, description }: StatisticsProps) {
   const statistics = [
     {
       icon: <Database />,
-      label: "data tersedia",
+      label: "Data tersedia",
       value: "100 resep data tersedia",
     },
     {
@@ -15,29 +20,23 @@ export default function RecipeStatistics() {
   ]
 
   return (
-    <section className="w-full px-5 mx-auto mt-10 lg:w-4/5 lg:px-0 lg:py-20">
-      <div className="flex flex-col items-center justify-center w-full gap-10 lg:flex-row">
+    <section className="mx-auto mt-10 w-full px-5 lg:w-4/5 lg:px-0 lg:py-20">
+      <div className="flex w-full flex-col items-center justify-center gap-10 lg:flex-row">
         <div className="hidden lg:block lg:w-1/3">
-          <img src="/img/makanan-arrival.png" alt="hero" />
+          <img src={img} alt="hero" />
         </div>
         <div className="lg:w-2/3">
-          <h1 className="pb-5 text-center text-black heading-mobile font-domine lg:text-left">
-            Daftar resep yang tersedia sekarang
-          </h1>
+          <h1 className="heading-mobile pb-5 text-center font-domine text-black lg:text-left">{heading}</h1>
           <div className="w-full lg:hidden ">
-            <img src="/img/makanan-arrival.png" alt="hero" />
+            <img src={img} alt="hero" />
           </div>
-          <p className="subheading-mobile">
-            <span className="py-10">Dapur Tradisional</span> menyediakan berbagai macam resep makanan ringan khas daerah
-            yang penuh cita rasa dan keunikan. Dengan resep-resep ini, Anda dapat menikmati kelezatan kuliner khas
-            daerah dengan mudah.
-          </p>
+          <p className="subheading-mobile">{description}</p>
 
           <div className="flex gap-10 pt-10">
             {statistics.map((stat, index) => (
               <div key={index} className="flex items-center gap-5">
                 {stat.icon}
-                <div className="flex flex-col subheading-mobile">
+                <div className="subheading-mobile flex flex-col">
                   <p className="text-gray-500">{stat.label}</p>
                   <p className="font-semibold text-black">{stat.value}</p>
                 </div>
