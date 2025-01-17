e8cb4ff4cecf23b9536cbbb48f46e2f3c76d0434
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-17 08:30:07 +0700

    feat: update TraditionalCuisineBenefits component to accept heading prop for dynamic content

--- src/components/Views/Home/TraditionalCuisineBenefits.tsx
+++ src/components/Views/Home/TraditionalCuisineBenefits.tsx
@@ -7,13 +7,13 @@ interface TraditionalProps {
   icon: LucideIcon
 }
 
-export default function TraditionalCuisineBenefits() {
+export default function TraditionalCuisineBenefits({ heading }: { heading: string }) {
   return (
-    <section className="flex flex-col items-center justify-center mx-auto mt-10 lg:mt-10 lg:w-4/5 lg:py-20">
+    <section className="mx-auto mt-10 flex flex-col items-center justify-center lg:mt-10 lg:w-4/5 lg:py-20">
       <h1 className="w-full py-10 text-center font-domine text-[1.5rem] leading-tight lg:w-1/2 lg:text-5xl">
-        Mengapa Masakan Tradisional Tak Tergantikan?
+        {heading}
       </h1>
-      <div className="grid grid-cols-2 overflow-hidden bg-white border shadow-lg rounded-2xl md:grid-cols-3 ">
+      <div className="grid grid-cols-2 overflow-hidden rounded-2xl border bg-white shadow-lg md:grid-cols-3 ">
         {ListTraditionalCuisineBenefits.map(({ title, description, icon: IconComponent }: TraditionalProps, index) => (
           <div
             key={title}
@@ -21,8 +21,8 @@ export default function TraditionalCuisineBenefits() {
               index < 3 ? "border-b" : ""
             } ${index % 3 !== 2 ? "md:border-r" : ""}`}
           >
-            <div className="flex items-center justify-center w-16 h-16 shadow-xl rounded-xl">
-              <IconComponent className="w-8 h-8 text-black" />
+            <div className="flex h-16 w-16 items-center justify-center rounded-xl shadow-xl">
+              <IconComponent className="h-8 w-8 text-black" />
             </div>
 
             <h2 className="my-6 text-[1rem] font-semibold text-black lg:text-lg">{title}</h2>
