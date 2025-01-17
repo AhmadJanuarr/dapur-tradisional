0cf5cb80dfce9215dfdd9d8eb4de8e759f5106f2
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-17 08:29:55 +0700

    feat: add HomePage component with HeroSection, TraditionalCuisineBenefits, RecipeStatistics, and SupportFaq

---
+++ src/pages/public/Home.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import HeroSection from "@/components/Views/Home/Hero"
+import TraditionalCuisineBenefits from "@/components/Views/Home/TraditionalCuisineBenefits"
+import RecipeStatistics from "@/components/Views/Home/RecipeStatistics"
+import SupportFaq from "@/components/Views/Home/SupportFaq"
+
+export default function HomePage() {
+  return (
+    <React.Fragment>
+      <HeroSection
+        heading="Masak Dengan Lebih Mudah & Kreatif."
+        description="Dengan Dapur Tradisional, Anda bisa menemukan inspirasi untuk masakan lezat setiap hari. Cari resep favorit Anda sekarang!"
+        callToAction="Jelajahi Resep Terbaru"
+        to="/recipes"
+        href="https://github.com/AhmadJanuarr/recipe-managements"
+      />
+      <TraditionalCuisineBenefits heading="Mengapa Masakan Tradisional Tak Tergantikan?" />
+      <RecipeStatistics
+        heading="Daftar resep yang tersedia sekarang"
+        img="/img/makanan-arrival.png"
+        description="Dapur Tradisional menyediakan berbagai macam resep makanan ringan khas daerah yang penuh cita rasa dan keunikan. Dengan resep-resep ini, Anda dapat menikmati kelezatan kuliner khas daerah dengan mudah."
+      />
+      <SupportFaq
+        heading="Pertanyaan yang Sering Diajukan"
+        subheading="Jawaban untuk Pertanyaan Umum tentang Resep dan Penggunaan Website"
+      />
+    </React.Fragment>
+  )
+}
