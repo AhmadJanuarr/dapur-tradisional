9f3b72971c3b9d39cf6f4d91c9cb09287c453601
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-21 00:00:00 +0700

    refactor: update TraditionalCuisineBenefits component styles for improved readability and consistency

--- src/components/Views/Home/TraditionalCuisineBenefits.tsx
+++ src/components/Views/Home/TraditionalCuisineBenefits.tsx
@@ -10,9 +10,7 @@ interface TraditionalProps {
 const TraditionalCuisineBenefits = ({ heading }: { heading: string }) => {
   return (
     <section className="mx-auto mt-10 flex flex-col items-center justify-center lg:mt-10 lg:w-4/5 lg:py-20">
-      <h1 className="w-full py-10 text-center font-domine text-[1.5rem] leading-tight lg:w-1/2 lg:text-5xl">
-        {heading}
-      </h1>
+      <h1 className="heading w-full py-10 text-center font-domine leading-tight lg:w-1/2 ">{heading}</h1>
       <div className="grid grid-cols-2 overflow-hidden rounded-2xl border bg-white shadow-lg md:grid-cols-3 ">
         {ListTraditionalCuisineBenefits.map(({ title, description, icon: IconComponent }: TraditionalProps, index) => (
           <div
@@ -25,8 +23,8 @@ const TraditionalCuisineBenefits = ({ heading }: { heading: string }) => {
               <IconComponent className="h-8 w-8 text-black" />
             </div>
 
-            <h2 className="my-6 text-[1rem] font-semibold text-black lg:text-lg">{title}</h2>
-            <p className="mt-2 text-[0.8rem] text-black lg:text-sm ">{description}</p>
+            <h2 className="subheading my-6 font-semibold text-black ">{title}</h2>
+            <p className="subheading mt-2 text-black">{description}</p>
           </div>
         ))}
       </div>
