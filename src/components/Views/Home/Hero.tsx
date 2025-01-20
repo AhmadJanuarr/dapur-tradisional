f9400c25315b8a6c493d4e77c375f0cc3f7bf9ac
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-20 23:59:32 +0700

    refactor: improve layout and styling of HeroSection for better visual hierarchy and consistency

--- src/components/Views/Home/Hero.tsx
+++ src/components/Views/Home/Hero.tsx
@@ -12,23 +12,23 @@ type HeroSectionProps = {
 const HeroSection = ({ heading, description, callToAction, to, href }: HeroSectionProps) => {
   return (
     <section
-      className="flex h-screen w-full justify-center bg-cover bg-center bg-no-repeat"
+      className="flex justify-center w-full h-screen bg-center bg-no-repeat bg-cover"
       style={{ backgroundImage: 'url("/img/hero.jpg")' }}
     >
-      <div className="absolute z-20 h-screen w-full bg-black bg-opacity-70"></div>
+      <div className="absolute z-20 w-full h-screen bg-black bg-opacity-70"></div>
       <div className="z-30 flex items-center justify-center">
         <div className="w-3/4 text-center text-white">
           <h1 className="font-italian text-[2rem] font-normal lg:text-[5rem]">{heading}</h1>
-          <p className="subheading mt-6">{description}</p>
-          <div className="mt-10 flex justify-center gap-5">
+          <p className="mt-6 subheading">{description}</p>
+          <div className="flex justify-center gap-5 mt-10">
             <Link to={to}>
-              <Button className="border bg-white text-[0.8rem] text-black hover:border-white hover:text-white lg:py-5 lg:text-[1rem]">
+              <Button variant="secondary" className="text-black border subheading lg:py-5 ">
                 <Dessert />
                 {callToAction}
               </Button>
             </Link>
             <a href={href} target="_blank">
-              <Button variant="link" className="border text-[0.8rem] text-white lg:py-5 lg:text-[1rem]">
+              <Button variant="link" className="text-white border subheading lg:py-5">
                 Dokumentasi
               </Button>
             </a>
