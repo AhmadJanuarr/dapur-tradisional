348f36291e4602c3c6adb6fc7ed30b5876a2eb80
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-22 22:49:49 +0700

    feat: enhance Authentication component with improved layout and loading state handling

--- src/pages/auth/Authentication.tsx
+++ src/pages/auth/Authentication.tsx
@@ -3,41 +3,51 @@ import LoginForm from "@/components/Auth/Login/LoginForm"
 import SignupForm from "@/components/Auth/Signup/SignupForm"
 import { HeadingAuth } from "@/components/Auth/HeadingAuth"
 import { useState } from "react"
-import { Loading } from "@/components/Loading"
+import { LoadingFullScreen } from "@/components/Loading"
+import { ArrowLeft } from "lucide-react"
 
 export default function Authentication() {
   const [isLoading, setIsLoading] = useState(false)
   const pathname = useLocation().pathname
-
   const isLoginPage = pathname.includes("/auth/login")
   const isSignupPage = pathname.includes("/auth/register")
+  const headingText = isLoginPage ? "Masuk" : "Daftar"
+  const description = isLoginPage
+    ? "Masuk untuk menemukan resep favoritmu dan buat daftar resep dengan mudah"
+    : "Daftarkan akunmu untuk menemukan resep favoritmu"
+
+  const formComponent = () => {
+    if (isLoginPage) {
+      return <LoginForm setIsLoading={setIsLoading} />
+    } else if (isSignupPage) {
+      return <SignupForm isLoading={isLoading} setIsLoading={setIsLoading} />
+    }
+    return null
+  }
 
   return (
-    <section className="flex min-h-screen w-full justify-center gap-5 px-5 md:px-0">
+    <section className="flex justify-center w-full gap-5 px-5 md:px-0 lg:min-h-screen">
       <div className="hidden w-1/2 md:block">
-        <div className="h-screen w-full bg-gray-700">
-          <h1 className="p-10 text-3xl font-bold text-white">
-            <Link to={"/"}>ResepKita</Link>
+        <div className="w-full h-screen bg-center bg-cover" style={{ backgroundImage: "url('/img/bg-auth.png')" }}>
+          <h1 className="flex gap-2 p-10 text-3xl font-bold text-white">
+            <img src="/logo/logo-t.png" alt="logo" className="w-10" />
+            <Link to={"/"} className="flex items-center gap-2 text-black font-italian">
+              Dapur Tradisional
+            </Link>
           </h1>
         </div>
       </div>
-      <div className="flex w-full flex-col justify-center md:w-1/2">
+      <div className="flex flex-col justify-center w-full md:w-1/2">
         {isLoading ? (
-          <Loading />
+          <LoadingFullScreen />
         ) : (
-          <div className="flex w-full flex-col items-center">
-            <HeadingAuth
-              heading={isLoginPage ? "Masuk" : "Daftar"}
-              description={
-                isLoginPage
-                  ? "Masuk untuk menemukan resep favoritmu dan buat daftar resep dengan mudah"
-                  : "Daftarkan akunmu untuk menemukan resep favoritmu"
-              }
-            />
-            <div className="w-full md:w-1/2">
-              {isLoginPage && <LoginForm />}
-              {isSignupPage && <SignupForm isLoading={isLoading} setIsLoading={setIsLoading} />}
-            </div>
+          <div className="flex flex-col w-full lg:items-center">
+            <Link to={"/"} className="flex gap-2 py-3 text-left lg:hidden">
+              <ArrowLeft />
+              Kembali
+            </Link>
+            <HeadingAuth heading={headingText} description={description} />
+            <div className="w-full md:w-1/2">{formComponent()}</div>
           </div>
         )}
       </div>
