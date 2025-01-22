9055fb80d3148fd048cc81809738611579137ba0
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-22 22:22:55 +0700

    feat: implement role-based navigation and enhance login error handling

--- src/components/Auth/Login/LoginForm.tsx
+++ src/components/Auth/Login/LoginForm.tsx
@@ -8,31 +8,58 @@ import { Button } from "@/components/ui/button"
 import { AuthOption } from "@/components/Auth/AuthOption"
 import axios from "axios"
 import { toast } from "sonner"
+import { useNavigate } from "react-router-dom"
+import { jwtDecode } from "jwt-decode"
+import { useContext, useState } from "react"
+import { AuthContext } from "@/context/AuthContext"
+import { JwtPayload } from "jwt-decode"
 
-export default function LoginForm() {
+interface JwtPayloadWithRole extends JwtPayload {
+  role: string
+}
+export default function LoginForm({ setIsLoading }: { setIsLoading: React.Dispatch<React.SetStateAction<boolean>> }) {
+  const { login } = useContext(AuthContext)
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const navigate = useNavigate()
+
   const methods = useForm<z.infer<typeof formSchemaLogin>>({
     resolver: zodResolver(formSchemaLogin),
     defaultValues: { email: "", password: "" },
   })
 
-  async function onSubmit(values: z.infer<typeof formSchemaLogin>) {
+  const onSubmit = async (values: z.infer<typeof formSchemaLogin>) => {
+    setIsLoading(true)
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, values)
       const data = await response.data
-      if (data.success === true) {
-        toast.success("Login berhasil")
-        console.log("Navigating to Home...")
+      if (response.status === 200) {
+        localStorage.setItem("token", data.accessToken)
+        localStorage.setItem("user", JSON.stringify(data.data))
+        localStorage.setItem("profil", JSON.stringify(values))
+
+        const { accessToken } = await data
+        const decodeToken: JwtPayloadWithRole = jwtDecode(accessToken)
+        login({ accessToken, ...decodeToken })
+
+        if (decodeToken.role === "ADMIN") {
+          setIsLoading(false)
+          toast.success("Berhasil masuk sebagai admin")
+          navigate("/admin/dashboard")
+        } else if (decodeToken.role === "USER") {
+          toast.success("Berhasil masuk")
+          navigate("/recipes")
+        }
       }
-      console.log(data)
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
         const errorMessage = error.response.data.message
         if (errorMessage.includes("Pengguna tidak ditemukan")) {
           toast.error("Pengguna tidak ditemukan")
         } else {
-          toast.error("Terjadi kesalahan coba lagi")
+          toast.error("Terjadi kesalahan saat masuk")
         }
       }
+      setIsLoading(false)
     }
   }
 
@@ -52,11 +79,13 @@ export default function LoginForm() {
           placeholder="Password"
           inputType="password"
           formControl={methods.control}
+          setShowPassword={setShowPassword}
+          showPassword={showPassword}
         />
         <Button type="submit" className="w-full">
           Masuk
         </Button>
-        <AuthOption linkText="Belum punya akun?" linkTo="register" />
+        <AuthOption linkText="Belum punya akun? " linkTo="register" />
       </form>
     </Form>
   )
