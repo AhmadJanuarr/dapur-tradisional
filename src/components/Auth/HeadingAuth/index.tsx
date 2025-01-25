99b1eb989fcadb77665bcbf3457b03e766596fa2
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-25 22:57:07 +0700

    feat: add logo image to HeadingAuth component and update description styling

--- src/components/Auth/HeadingAuth/index.tsx
+++ src/components/Auth/HeadingAuth/index.tsx
@@ -1,8 +1,9 @@
 export function HeadingAuth({ heading, description }: { heading: string; description: string }) {
   return (
     <div className="my-5 flex flex-col items-center justify-center text-center">
+      <img src="/logo/logo-t.png" alt="logo" className="w-14" />
       <h2 className="text-3xl font-bold">{heading}</h2>
-      <p className="mt-2 text-center text-[0.8rem] md:w-2/3 md:text-[1rem]">{description}</p>
+      <p className="subheading-mobile mt-2 text-center md:w-2/3">{description}</p>
     </div>
   )
 }
