d6ea87144a19ff4820966b398876e56625140cfe
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:08:53 +0700

    feat: refactor AppLayout component to improve route handling and conditional rendering of header and footer

--- src/components/Layout/App/index.tsx
+++ src/components/Layout/App/index.tsx
@@ -6,21 +6,27 @@ import { Toaster } from "@/components/ui/sonner"
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const location = useLocation()
-  const availablePaths = ["/", "/about", "/contact", "/recipes"]
-  const pathname = availablePaths.includes(location.pathname)
-  const isAdminRoute = location.pathname.startsWith("/admin")
-  const isHome = "/".includes(location.pathname)
-  const isUserRoute = ["/auth/login", "/auth/register"].includes(location.pathname)
-  const mainWidth = isAdminRoute || isUserRoute || isHome ? "w-full" : "lg:w-4/5 w-full px-5 lg:px-0"
+  const pathname = location.pathname
+
+  const routes = {
+    availablePaths: ["/", "/about", "/contact", "/recipes"],
+    isAdminRoute: location.pathname.startsWith("/admin"),
+    isFullWidthRoute: ["/auth/login", "/auth/register", "/contact"].includes(location.pathname),
+    isHome: pathname === "/",
+  }
+  const showHeaderFooter = routes.availablePaths.includes(pathname) || pathname.startsWith("/recipes/")
+  const mainWidth =
+    routes.isAdminRoute || routes.isFullWidthRoute || routes.isHome ? "w-full" : "lg:w-4/5 w-full px-5 lg:px-0"
+
   return (
     <div className="flex w-full flex-col items-center justify-center font-inter">
       <div className="w-full">
-        {pathname && <Header />}
+        {showHeaderFooter && <Header />}
         <Toaster richColors />
         <main className="flex w-full items-center justify-center">
           <div className={mainWidth}>{children}</div>
         </main>
-        {pathname && <Footer />}
+        {showHeaderFooter && <Footer />}
       </div>
     </div>
   )
