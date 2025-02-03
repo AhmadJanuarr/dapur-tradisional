ebc6ef7b152ee5078d5de30bc6d627219f6ad7e3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:11:17 +0700

    style: improve layout and styling of RecipeStatistics component

--- src/components/Views/Home/RecipeStatistics.tsx
+++ src/components/Views/Home/RecipeStatistics.tsx
@@ -21,13 +21,13 @@ const RecipeStatistics = ({ heading, img, description }: StatisticsProps) => {
   ]
 
   return (
-    <section className="mx-auto mt-10 w-full px-5 lg:w-4/5 lg:px-0 lg:py-20">
-      <div className="flex w-full flex-col items-center justify-center gap-10 lg:flex-row">
+    <section className="w-full px-5 mx-auto mt-10 lg:w-4/5 lg:px-0 lg:py-20">
+      <div className="flex flex-col items-center justify-center w-full gap-10 lg:flex-row">
         <div className="hidden lg:block lg:w-1/3">
           <img src={img} alt="hero" />
         </div>
         <div className="lg:w-2/3">
-          <h1 className="heading pb-5 text-center font-domine text-black lg:text-left">{heading}</h1>
+          <h1 className="pb-5 font-semibold text-center text-black heading lg:text-left">{heading}</h1>
           <div className="w-full lg:hidden">
             <img src={img} alt="hero" />
           </div>
@@ -37,7 +37,7 @@ const RecipeStatistics = ({ heading, img, description }: StatisticsProps) => {
             {statistics.map((stat, index) => (
               <div key={index} className="flex items-center gap-5">
                 {stat.icon}
-                <div className="subheading flex flex-col">
+                <div className="flex flex-col subheading">
                   <p className="text-gray-500">{stat.label}</p>
                   <p className="font-semibold text-black">{stat.value}</p>
                 </div>
