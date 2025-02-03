f970bb9fba5ce5a25a9d6224946da1dec0566eb2
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:14:39 +0700

    refactor: clean up imports and improve layout in DetailRecipe component

--- src/pages/public/details/DetailRecipe.tsx
+++ src/pages/public/details/DetailRecipe.tsx
@@ -1,14 +1,12 @@
-// Import dependencies
-import ErrorRecipe from "@/pages/error/RecipeError" // Komponen untuk menampilkan error
-import { useQuery } from "@tanstack/react-query" // Hook untuk pengelolaan data async
-import axios from "axios" // Library untuk melakukan request HTTP
-import { BookIcon } from "lucide-react" // Icon untuk UI
-import { useParams } from "react-router-dom" // Hook untuk mendapatkan parameter dari URL
+import ErrorRecipe from "@/pages/error/RecipeError"
+import axios from "axios"
+import { useQuery } from "@tanstack/react-query"
+import { BookIcon } from "lucide-react"
+import { useParams } from "react-router-dom"
 
 export default function DetailRecipe() {
   const { id } = useParams()
   const apiUrl = import.meta.env.VITE_API_URL
-
   const fetchRecipeById = async () => {
     const response = await axios.get(`${apiUrl}/api/recipes/${id}`)
     return response.data
@@ -31,16 +29,20 @@ export default function DetailRecipe() {
   console.log(recipe)
 
   return (
-    <div className="mx-auto flex w-[80%] flex-col border">
-      <h1 className="py-2 text-3xl font-bold font-domine">{recipe.title}</h1>
-      <p className="pb-2 subheading">{recipe.description}</p>
+    <div className="mx-auto mt-28 flex w-[80%] flex-col">
+      <h1 className="py-2 font-domine text-3xl font-bold">{recipe.title}</h1>
+      <p className="subheading pb-2">{recipe.description}</p>
       <div className="flex items-center gap-2 py-4">
-        <BookIcon className="w-5 h-5 text-gray-700" />
+        <BookIcon className="h-5 w-5 text-gray-700" />
         <p>{recipe.category.replace("_", " ")}</p>
       </div>
 
       <figure>
-        <img src={`${apiUrl}/images/${recipe.image}`} alt={recipe.title} className="w-full shadow rounded-xl" />
+        <img
+          src={`${apiUrl}/images/${recipe.image}`}
+          alt={recipe.title}
+          className="h-[800px] w-full rounded-xl shadow"
+        />
         <figcaption>{recipe.title}</figcaption>
       </figure>
 
@@ -53,7 +55,6 @@ export default function DetailRecipe() {
         </ul>
       </div>
 
-      {/* Langkah-langkah */}
       <div className="mt-5">
         <h2 className="text-xl font-semibold">Langkah - Langkah:</h2>
         <ul className="ml-5 list-decimal">
