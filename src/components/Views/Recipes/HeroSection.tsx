3c1a05ab9232e16eb593ccbdccd3c399d04bb2d8
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:12:07 +0700

    style: refactor HeroSection component for improved layout and props handling

--- src/components/Views/Recipes/HeroSection.tsx
+++ src/components/Views/Recipes/HeroSection.tsx
@@ -1,35 +1,39 @@
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
 
-export default function HeroSection() {
+type HeroSectionProps = {
+  heading: string
+  subheading: string
+  image: string
+}
+
+const HeroSectionRecipe = ({ heading, subheading, image }: HeroSectionProps) => {
   return (
-    <div className="flex w-full mt-20">
-      <div className="flex w-full rounded-xl bg-[#F9802D]">
-        <div className="flex flex-col w-1/2 gap-2 pl-4 md:py-10 lg:py-32 lg:pl-8">
-          <h1 className="text-xl font-semibold text-white md:text-2xl xl:text-4xl">
-            Dapur tradisional - resep makanan khas daerah
-          </h1>
-          <p className="text-white text-primary">
-            Temukan resep makanan khas daerah Indonesia yang lezat dan mudah dibuat
-          </p>
-          <div className="relative ">
-            <div className="absolute transform -translate-y-1 pointer-events-none left-3 top-1/2">
-              <Search className="text-black" size={24} />
+    <section className="flex w-full lg:mt-28">
+      <div className="flex h-[500px] w-full flex-col-reverse gap-3 lg:flex-row ">
+        <div className="flex w-full flex-col justify-center rounded-xl bg-[#D6EFD8] px-4 py-4 text-black md:py-10 lg:w-[40%] lg:gap-5 lg:px-6 lg:py-32">
+          <h1 className="heading font-inter font-semibold">{heading}</h1>
+          <p className="subheading">{subheading}</p>
+          <div className="relative lg:mt-5">
+            <div className="-translate-y- pointer-events-none absolute left-3 top-1/2 transform">
+              <Search className="text-black" size={20} />
             </div>
             <Input
               type="search"
               placeholder="Cari resep..."
-              className="pl-12 mt-4 bg-white border-none rounded-md text- black placeholder:text-black"
+              className="mt-4 w-full rounded-md border-none bg-white pl-11 text-black placeholder:text-black"
               sonner-spinner
               aria-label="Search recipes"
             />
           </div>
         </div>
-
-        <div className="flex justify-center w-1/2">
-          <img src="/svg/cooking.svg" alt="cooking image" className="w-[200px] lg:w-[400px]" />
-        </div>
+        <div
+          className="-z-10 flex h-[200px] w-full justify-center rounded-xl bg-center bg-no-repeat object-cover lg:h-full lg:w-[60%]"
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
       </div>
-    </div>
+    </section>
   )
 }
+
+export default HeroSectionRecipe
