71f8286349d4d98e86dabd9effb47dd19b2cec2c
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:10:16 +0700

    feat: update ProfileSection component to include image source prop and enhance layout

--- src/components/Views/About/ProfileSection.tsx
+++ src/components/Views/About/ProfileSection.tsx
@@ -3,19 +3,20 @@ import HeadingSection from "./HeadingSection"
 type ProfileProps = {
   name: string
   role: string
+  src: string
 }
 
-const ProfileSection = ({ name, role }: ProfileProps) => {
+const ProfileSection = ({ name, role, src }: ProfileProps) => {
   return (
-    <section className="flex flex-col items-center justify-center w-full py-20">
-      <div className="absolute w-full mt-18 -z-10 h-3/5 bg-slate-50"></div>
+    <section className="flex w-full flex-col items-center justify-center py-20">
+      <div className="absolute -z-10 mt-3 h-1/2 w-full bg-slate-50 lg:mt-2 lg:h-3/5"></div>
       <HeadingSection
         heading="Meet Our Team"
         subHeading="Individu-individu berbakat di balik proyek-proyek kami yang sukses"
       />
       <figure>
-        <img src="/img/user.png" alt="user" className="w-[250px]  rounded-full py-12" />
-        <figcaption className="text-center subheading">
+        <img src={src} alt={name} className="mx-auto w-[150px] rounded-full py-4 lg:w-[250px] lg:py-12" />
+        <figcaption className="subheading text-center">
           <h2 className="font-semibold">{name}</h2>
           <p className="text-gray-500">{role}</p>
         </figcaption>
