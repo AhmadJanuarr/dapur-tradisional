010a505d8361788e3a001f79dc44908ef0fa5245
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:09:19 +0700

    feat: refactor RecipeSkeleton component for improved layout and responsiveness

--- src/components/Skeleton/recipeSkeleton.tsx
+++ src/components/Skeleton/recipeSkeleton.tsx
@@ -1,14 +1,14 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
-export default function RecipeSkeleton({ index }: { index: number }) {
+const RecipeSkeleton = ({ index }: { index: number }) => {
   return (
-    <div className="flex w-full flex-wrap">
+    <div className="flex w-full flex-col flex-wrap md:flex-row ">
       {Array(index)
         .fill(0)
         .map((_, i) => (
-          <div key={i} className="flex w-[25%] flex-col space-y-3 p-5">
+          <div key={i} className="flex w-full flex-col space-y-3 p-5 lg:w-[25%]">
             <Skeleton className="h-[300px] w-full rounded-xl" />
-            <div className="flex flex-col items-center justify-center space-y-2">
+            <div className="flex flex-col space-y-2">
               <Skeleton className="h-4 w-[250px]" />
               <Skeleton className="h-4 w-[200px]" />
               <div className="pt-5">
@@ -20,3 +20,5 @@ export default function RecipeSkeleton({ index }: { index: number }) {
     </div>
   )
 }
+
+export default RecipeSkeleton
