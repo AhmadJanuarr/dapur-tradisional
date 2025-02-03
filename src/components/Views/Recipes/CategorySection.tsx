b5a670730bd4b4088195cdbc99dc534c1f941f32
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:11:50 +0700

    style: refactor CategorySection and CardCategory components for improved layout and styling

--- src/components/Views/Recipes/CategorySection.tsx
+++ src/components/Views/Recipes/CategorySection.tsx
@@ -1,60 +1,37 @@
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { ChevronRight } from "lucide-react"
-import HeadingSection from "./HeadingSection"
+import { ArrowRight } from "lucide-react"
 
-// Komponen untuk menampilkan kategori kartu
-function CardCategory({ title, content, src }: { title: string; content: string; src: string }) {
+const CardCategory = ({ title, src, color }: { title: string; src: string; color: string }) => {
   return (
-    <Card
-      className={`relative flex h-[600px] w-1/3 items-center justify-between rounded-xl border-none bg-cover bg-center bg-no-repeat`}
-      style={{ backgroundImage: `url(${src})` }}
+    <div
+      className={`relative flex h-[150px] items-center justify-between overflow-hidden rounded-xl border-none bg-cover bg-center bg-no-repeat lg:h-[250px] lg:w-1/3`}
+      style={{ backgroundColor: `${color}` }}
     >
-      <div className="absolute w-full h-full bg-black rounded-xl bg-opacity-30" />
-      <div className="z-10 text-white">
-        <CardHeader>
-          <CardTitle className="text-3xl">{title}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-lg">{content}</p>
-        </CardContent>
-        <CardFooter>
-          <Button variant="link" className="p-0 text-white text-primary">
-            Lihat Resep
-            <ChevronRight />
+      <div className="z-10 flex w-full items-center justify-between px-5">
+        <div className="z-20 flex w-1/2 flex-col gap-5">
+          <h1 className="font-inter text-xl font-semibold lg:text-3xl">{title}</h1>
+          <Button className="w-10 lg:w-12" variant="outline">
+            <ArrowRight />
           </Button>
-        </CardFooter>
+        </div>
+        <figure className="absolute -right-14 top-10">
+          <img src={src} alt="kategori" className=" w-[10rem] object-cover lg:w-[20rem]" />
+        </figure>
       </div>
-    </Card>
+    </div>
   )
 }
 
-// Komponen untuk menampilkan bagian kategori
-export default function CategorySection() {
+const CategorySection = () => {
   return (
-    <section className="w-full">
-      <HeadingSection
-        heading="Kategori Resep"
-        description="Temukan resep favoritmu berdasarkan kategori"
-        isShowButton={false}
-      />
-      <div className="flex justify-center w-full gap-2">
-        <CardCategory
-          title="Makanan Ringan (Cemilan)"
-          content="Cemilan unik dengan rasa dan tekstur khas Nusantara."
-          src="/img/resep/klepon.webp"
-        />
-        <CardCategory
-          title="Makanan Utama (Berat)"
-          content="Hidangan penuh cita rasa khas dari berbagai daerah Indonesia."
-          src="/img/resep/ayam-betutu.webp"
-        />
-        <CardCategory
-          title="Kue Tradisional"
-          content="Kue autentik yang menghadirkan rasa khas budaya Indonesia."
-          src="/img/resep/serabi.webp"
-        />
+    <section className="mt-10 w-full lg:mt-3">
+      <div className="flex w-full flex-col justify-center gap-2 lg:flex-row ">
+        <CardCategory title="Makanan Ringan (Cemilan)" color="#D0E7D2" src="/logo/kategori-ringan.png" />
+        <CardCategory title="Makanan Utama (Berat)" color="#D0E7D2" src="/logo/kategori-berat.png" />
+        <CardCategory title="Kue Tradisional" color="#D0E7D2" src="/logo/kategori-kue.png" />
       </div>
     </section>
   )
 }
+
+export default CategorySection
