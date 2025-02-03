560c826403a1b8b0851064296b139711dd399633
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:14:11 +0700

    feat: add ContactPage component with HeroSection for user inquiries

---
+++ src/pages/public/Contact.tsx
@@ -0,0 +1,10 @@
+import HeroSectionContact from "@/components/Views/Contact/HeroSection"
+import { Fragment } from "react/jsx-runtime"
+
+export default function ContactPage() {
+  return (
+    <Fragment>
+      <HeroSectionContact />
+    </Fragment>
+  )
+}
