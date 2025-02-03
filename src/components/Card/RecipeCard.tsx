b0fb7bba6aafa155b437919bc245d35b5c991245
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:08:34 +0700

    feat: enhance RecipeCard component with favorite button and improved layout

--- src/components/Card/RecipeCard.tsx
+++ src/components/Card/RecipeCard.tsx
@@ -1,22 +1,46 @@
 import { Button } from "@/components/ui/button"
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { ChevronRight } from "lucide-react"
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { ChevronRight, Heart } from "lucide-react"
 
-export default function RecipeCard({ image, title, category }: { image: string; title: string; category: string }) {
+type RecipeCardProps = {
+  image: string
+  title: string
+  category: string
+  handleClick?: () => void
+}
+
+const RecipeImage = ({ image, title }: { image: string; title: string }) => (
+  <div className="3xl:h-[300px] mx-auto h-[250px] w-[250px] lg:h-[250px]">
+    <img src={image} className="h-full w-full rounded-full object-cover shadow-xl" alt={title} />
+  </div>
+)
+
+const FavoriteButton = () => (
+  <div className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow">
+    <Heart className="text-slate-400" size={25} />
+  </div>
+)
+
+export default function RecipeCard({ image, title, category, handleClick = () => {} }: RecipeCardProps) {
   return (
-    <Card className="flex w-[25%] flex-col items-center border-none text-center shadow-none">
-      <CardHeader>
-        <div className="h-[300px]">
-          <img src={image} className="h-full w-full object-cover" alt={title} />
-        </div>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{category.replace("_", " ")}</CardDescription>
+    <Card className="relative flex w-full flex-col rounded-xl border-none bg-slate-100 p-1 shadow-none md:w-[48.8%] lg:w-[24%]">
+      <CardHeader className="relative">
+        <RecipeImage image={image} title={title} />
+        <FavoriteButton />
       </CardHeader>
-      <CardFooter>
-        <Button variant="link">
+      <div className="flex flex-col justify-between gap-2 rounded-xl border border-slate-200 bg-white px-5 py-2 lg:h-[170px]">
+        <div className="flex flex-col gap-2">
+          <CardDescription>{category.replace("_", " ")}</CardDescription>
+          <CardTitle className="mb-2 lg:text-2xl">{title}</CardTitle>
+        </div>
+        <Button
+          onClick={handleClick}
+          variant="outline"
+          className="w-[50%] border-[#31ac5d] text-[#31ac5d] hover:bg-[#31ac5d] hover:text-white"
+        >
           Lihat Resep <ChevronRight />
         </Button>
-      </CardFooter>
+      </div>
     </Card>
   )
 }
