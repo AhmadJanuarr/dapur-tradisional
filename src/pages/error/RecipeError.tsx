b540415af5a480706ed31324383cc63ade00a4db
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:13:57 +0700

    refactor: simplify ErrorRecipe component structure and improve readability

--- src/pages/error/RecipeError.tsx
+++ src/pages/error/RecipeError.tsx
@@ -2,19 +2,18 @@ import { Button } from "@/components/ui/button"
 import { ArrowLeft, Repeat } from "lucide-react"
 import { Link } from "react-router-dom"
 
-const ErrorRecipe = () => {
+export default function ErrorRecipe() {
   const handleClick = () => {
     window.location.reload()
   }
 
   return (
-    <section className="flex flex-col items-center justify-center w-full h-screen px-5 my-20">
+    <section className="my-2 flex w-full flex-col items-center justify-center px-5">
       <div className="flex flex-col items-center text-center lg:w-1/2">
         <figure>
           <img src="/img/error-recipe.png" alt="Ilustrasi kesalahan saat memuat resep" className="mx-auto" />
           <figcaption className="sr-only">Ilustrasi kesalahan memuat resep</figcaption>
         </figure>
-
         <header>
           <h1 className="text-[1.5rem] font-bold lg:text-xl">Tidak Dapat Memuat Resep</h1>
           <p className="subheading">
@@ -22,29 +21,25 @@ const ErrorRecipe = () => {
             pemeliharaan server.
           </p>
         </header>
-
-        <div className="flex gap-4 mt-5">
-          <Button className="rounded-md subheading" onClick={handleClick}>
+        <div className="mt-5 flex gap-4">
+          <Button className="subheading rounded-md" onClick={handleClick}>
             <Repeat />
             Coba lagi
           </Button>
           <Link to="/">
-            <Button className="rounded-md subheading" variant="secondary">
+            <Button className="subheading rounded-md" variant="secondary">
               <ArrowLeft />
               Kembali ke beranda
             </Button>
           </Link>
         </div>
-
         <footer className="mt-5">
-          <p className="text-gray-500 subheading">
+          <p className="subheading text-gray-500">
             Masih mengalami masalah?
-            <span className="font-semibold text-black cursor-pointer"> Hubungi Dukungan</span>
+            <span className="cursor-pointer font-semibold text-black"> Hubungi Dukungan</span>
           </p>
         </footer>
       </div>
     </section>
   )
 }
-
-export default ErrorRecipe
