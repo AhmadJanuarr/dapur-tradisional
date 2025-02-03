667b1a64fe830adff6b28a5fdf1cef06f8ea3e08
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-03 22:11:35 +0700

    style: enhance layout and styling of TraditionalCuisineBenefits component for better presentation

--- src/components/Views/Home/TraditionalCuisineBenefits.tsx
+++ src/components/Views/Home/TraditionalCuisineBenefits.tsx
@@ -9,17 +9,17 @@ interface TraditionalProps {
 
 const TraditionalCuisineBenefits = ({ heading }: { heading: string }) => {
   return (
-    <section className="mx-auto mt-10 flex flex-col items-center justify-center lg:mt-10 lg:w-4/5 lg:py-20">
-      <h1 className="heading w-full py-10 text-center font-domine leading-tight lg:w-1/2 ">{heading}</h1>
+    <section className="mx-auto mt-10 flex flex-col items-center justify-center px-5 lg:mt-10 lg:w-4/5 lg:py-20">
+      <h1 className="heading w-full py-10 text-center font-semibold leading-tight lg:w-1/2">{heading}</h1>
       <div className="grid grid-cols-2 overflow-hidden rounded-2xl border bg-white shadow-lg md:grid-cols-3 ">
         {ListTraditionalCuisineBenefits.map(({ title, description, icon: IconComponent }: TraditionalProps, index) => (
           <div
             key={title}
-            className={`flex flex-col  justify-start border p-6 ${
+            className={`flex flex-col justify-start border p-6 ${
               index < 3 ? "border-b" : ""
             } ${index % 3 !== 2 ? "md:border-r" : ""}`}
           >
-            <div className="flex h-16 w-16 items-center justify-center rounded-xl shadow-xl">
+            <div className="flex h-16 w-16 items-center justify-center rounded-xl border">
               <IconComponent className="h-8 w-8 text-black" />
             </div>
 
