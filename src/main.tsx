620f27a8273d6dca9f2159e4faa43bcd1e97e452
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-18 23:38:34 +0700

    feat: wrap App component with DarkModeTheme for improved theming support

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.tsx"
 import { AuthProvider } from "./context/AuthContext.tsx"
+import App from "./App.tsx"
+import DarkModeTheme from "./context/Themes.tsx"
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <DarkModeTheme>
+        <App />
+      </DarkModeTheme>
     </AuthProvider>
   </StrictMode>,
 )
