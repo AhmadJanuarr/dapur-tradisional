60f4a90f00ee2ffd0b083b0ba7c0879299975ea1
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:57:23 +0700

    feat: add BookmarkButton component for enhanced user interaction

---
+++ src/components/Button/Bookmark.tsx
@@ -0,0 +1,7 @@
+import { Bookmark } from "lucide-react"
+
+export const BookMarksButton = () => (
+  <div className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow">
+    <Bookmark className="text-[#f97316]" size={25} />
+  </div>
+)
