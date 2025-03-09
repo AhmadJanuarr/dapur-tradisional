4e142fad0c2bf9662f43eedaf932426c00c57142
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:43:39 +0700

    feat: integrate useRecipeForm hook for improved recipe submission handling

--- src/components/Admin/RecipeForm.tsx
+++ src/components/Admin/RecipeForm.tsx
@@ -1,72 +1,21 @@
 import { Form } from "@/components/ui/form"
-import { useFieldArray, useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import { RecipeFormValues } from "@/types/RecipeForm.types"
-import axios, { AxiosError } from "axios"
-import { toast } from "sonner"
 import { FormSelect } from "./Form/FormSelect"
 import { FormImage } from "./Form/FormImage"
 import { FormInput } from "./Form/FormInput"
 import { FormArray } from "./Form/FormArray"
 import { FormColumn } from "./Form/FormColumn"
+import { Loader2 } from "lucide-react"
+import { useRecipeForm } from "@/api/useRecipeForm"
 
-export default function RecipeForm({ className }: { className?: string }) {
-  const form = useForm<RecipeFormValues>({
-    defaultValues: {
-      title: "",
-      description: "",
-      image: "",
-      category: "",
-      ingredients: [],
-      steps: [],
-      tips: "",
-      difficulty: "",
-      calories: 0,
-      protein: 0,
-      carbs: 0,
-      fat: 0,
-    },
-  })
-  const { control, handleSubmit, register, reset } = form
-  const ingredientsArray = useFieldArray({
-    control,
-    name: "ingredients",
-  })
-
-  const stepsArray = useFieldArray({
-    control,
-    name: "steps",
-  })
-
-  const onSubmit = handleSubmit(async (values) => {
-    const formData = new FormData()
-
-    formData.append("title", values.title)
-    formData.append("description", values.description)
-    formData.append("category", values.category)
-    if (values.image) formData.append("image", values.image)
-    formData.append("ingredients", JSON.stringify(values.ingredients))
-    formData.append("steps", JSON.stringify(values.steps))
-    formData.append("tips", values.tips)
-    formData.append("difficulty", values.difficulty)
-    formData.append("calories", String(Number(values.calories) || 0))
-    formData.append("protein", String(Number(values.protein) || 0))
-    formData.append("carbs", String(Number(values.carbs) || 0))
-    formData.append("fat", String(Number(values.fat) || 0))
-
-    console.log(values)
-    try {
-      await axios.post(`${import.meta.env.VITE_API_URL}/api/recipes`, formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-      toast.success("Resep berhasil ditambahkan")
-      reset()
-    } catch (error) {
-      const axiosError = error as AxiosError
-      console.log(axiosError.response)
-      toast.error("Terjadi kesalahan coba lagi")
-    }
-  })
+export default function RecipeForm({
+  className,
+  setOpenForm,
+}: {
+  className?: string
+  setOpenForm: React.Dispatch<React.SetStateAction<boolean>>
+}) {
+  const { form, ingredientsArray, stepsArray, onSubmit, register, control } = useRecipeForm(setOpenForm)
   return (
     <Form {...form}>
       <form
@@ -149,8 +98,8 @@ export default function RecipeForm({ className }: { className?: string }) {
           <FormInput name="tips" label="Tips" placeholder="Tips" control={control} type="text" isTextArea />
         </div>
         <div className="my-6 flex w-full sm:justify-center">
-          <Button type="submit" variant="default" className="rounded-md">
-            Simpan Resep
+          <Button type="submit" variant="default" className="rounded-md" disabled={form.formState.isSubmitting}>
+            {form.formState.isSubmitting ? <Loader2 className="animate-spin" /> : "Simpan Resep"}
           </Button>
         </div>
       </form>
