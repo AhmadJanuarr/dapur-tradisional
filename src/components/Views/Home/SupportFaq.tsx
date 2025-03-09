7b75211159343230edcb88be41afa7ba4188337f
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:05:43 +0700

    style: improve layout and accessibility of SupportFaq component

--- src/components/Views/Home/SupportFaq.tsx
+++ src/components/Views/Home/SupportFaq.tsx
@@ -15,17 +15,21 @@ interface SupportFaqSectionProps {
 
 const SupportFaq = ({ heading, subheading }: SupportFaqSectionProps) => {
   return (
-    <section className="flex flex-col items-center justify-center px-5 py-20 mx-auto mt-10 lg:w-4/5 lg:px-0">
+    <section className="mx-auto mt-10 flex flex-col items-center justify-center px-5 py-20 lg:w-11/12 lg:px-0 2xl:w-4/5">
       <header className="w-full">
-        <h1 className="pb-5 font-semibold text-black text heading">{heading}</h1>
-        <p className="text-gray-500 subheading">{subheading}</p>
+        <h1 className="text heading pb-5 font-semibold text-black dark:text-white">{heading}</h1>
+        <p className="subheading text-gray-500 dark:text-white">{subheading}</p>
       </header>
 
-      <div className="grid w-full grid-cols-1 gap-4 py-10 place-items-center md:grid-cols-2 xl:grid-cols-2">
+      <div className="grid w-full grid-cols-1 place-items-center gap-4 py-10 md:grid-cols-2 xl:grid-cols-2">
         {SupportsFaqList.map(({ question, answer }: SupportFaqProps, index) => (
           <div className="w-full" key={index}>
-            <Accordion type="single" collapsible className="w-full px-2 bg-gray-100 rounded-md lg:px-4">
-              <AccordionItem value={`item-${index}`} className="border-none subheading">
+            <Accordion
+              type="single"
+              collapsible
+              className="w-full rounded-md bg-gray-100 px-2 dark:bg-darkBackground lg:px-4"
+            >
+              <AccordionItem value={`item-${index}`} className="subheading border-none dark:text-white">
                 <AccordionTrigger>{question}</AccordionTrigger>
                 <AccordionContent>{answer}</AccordionContent>
               </AccordionItem>
