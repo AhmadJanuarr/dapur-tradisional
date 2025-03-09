a55822d1a3e6a5160ae5e4d692c0975da92c53fc
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:07:06 +0700

    feat: update AdminPanel to use ManageRecipes component instead of RecipesPages

--- src/pages/admin/AdminPanel.tsx
+++ src/pages/admin/AdminPanel.tsx
@@ -1,14 +1,14 @@
+import { useLocation } from "react-router-dom"
 import DashboardPage from "./DashboardPage"
 import AppLayout from "@/components/Admin/AppLayout"
-import RecipesPages from "./RecipesPages"
-import { useLocation } from "react-router-dom"
+import ManageRecipes from "./ManageRecipes"
 
 export default function AdminPanel() {
   const location = useLocation().pathname
   return (
     <AppLayout>
       {location.includes("/admin/dashboard") && <DashboardPage />}
-      {location.includes("/admin/recipes") && <RecipesPages />}
+      {location.includes("/admin/recipes") && <ManageRecipes />}
       {location.includes("/admin/users") && <h1>Dashboard</h1>}
     </AppLayout>
   )
