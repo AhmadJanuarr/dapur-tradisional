82e3ffcaef0462e0df014f31584aa0bf864e727b
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:08:58 +0700

    refactor: improve formatting and structure of form schemas in FormSchema.ts

--- src/schemas/FormSchema.ts
+++ src/schemas/FormSchema.ts
@@ -1,33 +1,44 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const formSchemaRegister = z.object({
-    name: z.string().min(6, {
-        message : "Nama minimal 6 karakter",
-    }).max(50,{
-        message : "Nama maksimal 50 karakter",
+  name: z
+    .string()
+    .min(6, {
+      message: "Nama minimal 6 karakter",
+    })
+    .max(50, {
+      message: "Nama maksimal 50 karakter",
     }),
-    email: z.string().min(1, {
-        message : "Email harus tidak boleh kosong",
-    }).email(),
-    password: z.string().min(8,{
-        message : "Password minimal 8 karakter",
-    }),
+  email: z
+    .string()
+    .min(1, {
+      message: "Email harus tidak boleh kosong",
+    })
+    .email(),
+  password: z.string().min(8, {
+    message: "Password minimal 8 karakter",
+  }),
 })
 
 export const formSchemaLogin = z.object({
-    email: z.string().min(1,{
-        message : "Email harus tidak boleh kosong",
-    }).email(),
-    password: z.string().min(8, {
-        message : "Password minimal 8 karakter",
+  email: z
+    .string()
+    .min(1, {
+      message: "Email harus tidak boleh kosong",
+    })
+    .email({
+      message: "Email tidak valid",
     }),
+  password: z.string().min(8, {
+    message: "Password minimal 8 karakter",
+  }),
 })
 
 export const formSchemaRecipe = z.object({
-    title: z.string().max(80),
-    description: z.string().max(500),
-    image: z.string(),
-    category: z.string(),
-    ingredients: z.array(z.string().max(100)),
-    steps: z.array(z.string().max(500)),
+  title: z.string().max(80),
+  description: z.string().max(500),
+  image: z.string(),
+  category: z.string(),
+  ingredients: z.array(z.string().max(100)),
+  steps: z.array(z.string().max(500)),
 })
