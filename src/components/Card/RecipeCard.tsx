685f16b6c89cbad8b3a8aa5c9db73bb72439fc5a
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:58:01 +0700

    feat: enhance RecipeCard component with bookmark functionality and improved layout

--- src/components/Card/RecipeCard.tsx
+++ src/components/Card/RecipeCard.tsx
@@ -1,44 +1,42 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { ChevronRight, Heart } from "lucide-react"
+import { ChevronRight } from "lucide-react"
+import { BookMarksButton } from "../Button/Bookmark"
+import { useState } from "react"
+import { Recipe } from "@/types/Recipe.types"
 
 type RecipeCardProps = {
   image: string
   title: string
   category: string
-  handleClick?: () => void
+  handleClickViewDetail?: () => void
 }
 
 const RecipeImage = ({ image, title }: { image: string; title: string }) => (
-  <div className="3xl:h-[300px] mx-auto h-[250px] w-[250px] lg:h-[250px]">
+  <div className="mx-auto h-36 w-36 lg:h-[300px] lg:w-[300px]">
     <img src={image} className="h-full w-full rounded-full object-cover shadow-xl" alt={title} />
   </div>
 )
 
-const FavoriteButton = () => (
-  <div className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow">
-    <Heart className="text-slate-400" size={25} />
-  </div>
-)
-
-export default function RecipeCard({ image, title, category, handleClick = () => {} }: RecipeCardProps) {
+export default function RecipeCard({ image, title, category, handleClickViewDetail }: RecipeCardProps) {
+  const [bookmarks, setBookmarks] = useState<Recipe[]>([])
   return (
-    <Card className="relative flex w-full flex-col rounded-xl border-none bg-slate-100 p-1 shadow-none md:w-[48.8%] lg:w-[24%]">
-      <CardHeader className="relative">
+    <Card className="relative flex w-full flex-col rounded-xl border-none bg-slate-100 shadow-none dark:bg-darkBackground">
+      <CardHeader className="relative overflow-hidden">
         <RecipeImage image={image} title={title} />
-        <FavoriteButton />
+        <BookMarksButton />
       </CardHeader>
-      <div className="flex flex-col justify-between gap-2 rounded-xl border border-slate-200 bg-white px-5 py-2 lg:h-[170px]">
-        <div className="flex flex-col gap-2">
+      <div className="flex flex-col justify-between gap-2 rounded-xl border border-slate-200 bg-white px-2 py-2 lg:h-[180px] lg:px-6 lg:py-4">
+        <div className="flex flex-col gap-2 dark:text-black">
           <CardDescription>{category.replace("_", " ")}</CardDescription>
           <CardTitle className="mb-2 lg:text-2xl">{title}</CardTitle>
         </div>
         <Button
-          onClick={handleClick}
+          onClick={handleClickViewDetail}
           variant="outline"
-          className="w-[50%] border-[#31ac5d] text-[#31ac5d] hover:bg-[#31ac5d] hover:text-white"
+          className="rounded-xl border-[#ffeddf] px-10 text-[#f97316] hover:bg-[#f97316] hover:text-white md:w-[50%]"
         >
-          Lihat Resep <ChevronRight />
+          <p className="subheading">Lihat Resep</p> <ChevronRight />
         </Button>
       </div>
     </Card>
