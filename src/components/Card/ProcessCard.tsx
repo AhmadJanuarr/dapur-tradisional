1e93e5e9102e4e7aa10f9204a799ed6e81460a37
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:57:49 +0700

    style: update ProcessCard component for improved dark mode support and visual consistency

--- src/components/Card/ProcessCard.tsx
+++ src/components/Card/ProcessCard.tsx
@@ -5,10 +5,12 @@ type ProcessCardProps = {
 }
 const ProcessCard = ({ index, title, desc }: ProcessCardProps) => {
   return (
-    <div className="subheading h relative flex h-[150px] w-full flex-col gap-4 rounded-xl border p-5 pt-5 md:h-[250px] lg:h-[220px] lg:w-1/4">
-      <div className="absolute -top-3 left-5 rounded-full bg-black px-2 text-white">{index}</div>
+    <div className="subheading h relative flex h-[150px] w-full flex-col gap-4 rounded-xl border p-5 pt-5 dark:border-gray-50 md:h-[250px] lg:h-[220px] lg:w-1/4">
+      <div className="bg-darkPrimary absolute -top-3 left-5 rounded-full px-2 text-white dark:bg-white dark:text-black">
+        {index}
+      </div>
       <h1 className="font-semibold">{title}</h1>
-      <p className="text-gray-500">{desc}</p>
+      <p className="text-gray-500 dark:text-white">{desc}</p>
     </div>
   )
 }
