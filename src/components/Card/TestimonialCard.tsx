7a0671e30a2af7521d91b1a3520b4f3c6ea7b6d0
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:58:23 +0700

    style: enhance TestimonialCard component for improved dark mode support

--- src/components/Card/TestimonialCard.tsx
+++ src/components/Card/TestimonialCard.tsx
@@ -6,15 +6,15 @@ type TestimonialCardProps = {
 }
 const TestimonialCard = ({ img, name, role, desc }: TestimonialCardProps) => {
   return (
-    <div className="subheading mr-5 flex w-[350px] flex-col gap-4 rounded-lg bg-slate-50 p-5 lg:h-[180px] lg:w-[500px]">
+    <div className="subheading dark:bg-darkBackground mr-5 flex w-[350px] flex-col gap-4 rounded-lg bg-slate-50 p-5 lg:h-[180px] lg:w-[500px]">
       <div className="flex items-center gap-4">
-        <img src={img} alt={name} className="h-10 w-10 rounded-full bg-black" />
-        <div className="flex flex-col">
+        <img src={img} alt={name} className="h-10 w-10 rounded-full" />
+        <div className="flex flex-col dark:text-white">
           <h3 className="font-semibold">{name}</h3>
-          <p className="text-gray-500">{role}</p>
+          <p className="text-gray-500 dark:text-white">{role}</p>
         </div>
       </div>
-      <p className="text-gray-500">{desc}</p>
+      <p className="text-gray-500 dark:text-white">{desc}</p>
     </div>
   )
 }
