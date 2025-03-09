b1c13be14f016bf6fbc47be4a04e74502435a8e5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:05:57 +0700

    style: enhance CategorySection component with improved layout and navigation links

--- src/components/Views/Recipes/CategorySection.tsx
+++ src/components/Views/Recipes/CategorySection.tsx
@@ -1,18 +1,21 @@
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
+import { Link } from "react-router-dom"
 
-const CardCategory = ({ title, src, color }: { title: string; src: string; color: string }) => {
+const CardCategory = ({ title, src, color, to }: { title: string; src: string; color: string; to: string }) => {
   return (
     <div
       className={`relative flex h-[150px] items-center justify-between overflow-hidden rounded-xl border-none bg-cover bg-center bg-no-repeat lg:h-[250px] lg:w-1/3`}
       style={{ backgroundColor: `${color}` }}
     >
       <div className="z-10 flex w-full items-center justify-between px-5">
-        <div className="z-20 flex w-1/2 flex-col gap-5">
-          <h1 className="font-inter text-xl font-semibold lg:text-3xl">{title}</h1>
-          <Button className="w-10 lg:w-12" variant="outline">
-            <ArrowRight />
-          </Button>
+        <div className="z-20 flex w-2/3 flex-col gap-5">
+          <h1 className="font-inter text-xl font-semibold text-black lg:text-3xl">{title}</h1>
+          <Link to={to}>
+            <Button className="w-10 lg:w-12" variant="default">
+              <ArrowRight />
+            </Button>
+          </Link>
         </div>
         <figure className="absolute -right-14 top-10">
           <img src={src} alt="kategori" className=" w-[10rem] object-cover lg:w-[20rem]" />
@@ -26,9 +29,19 @@ const CategorySection = () => {
   return (
     <section className="mt-10 w-full lg:mt-3">
       <div className="flex w-full flex-col justify-center gap-2 lg:flex-row ">
-        <CardCategory title="Makanan Ringan (Cemilan)" color="#D0E7D2" src="/logo/kategori-ringan.png" />
-        <CardCategory title="Makanan Utama (Berat)" color="#D0E7D2" src="/logo/kategori-berat.png" />
-        <CardCategory title="Kue Tradisional" color="#D0E7D2" src="/logo/kategori-kue.png" />
+        <CardCategory
+          title="Makanan Ringan (Cemilan)"
+          color="#ffeddf"
+          src="/logo/kategori-ringan.png"
+          to="/recipes/category/Makanan_Ringan"
+        />
+        <CardCategory
+          title="Makanan Utama (Berat)"
+          color="#ffeddf"
+          src="/logo/kategori-berat.png"
+          to="/recipes/category/Makanan_Berat "
+        />
+        <CardCategory title="Kue Tradisional" color="#ffeddf" src="/logo/kategori-kue.png" to="/recipes/category/Kue" />
       </div>
     </section>
   )
