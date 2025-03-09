6f33f467ac5ee18613351434f05adf4ed8141816
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:45:37 +0700

    feat: enhance FormSelect component to accept dynamic categories for improved flexibility

--- src/components/Admin/Form/FormSelect.tsx
+++ src/components/Admin/Form/FormSelect.tsx
@@ -1,31 +1,31 @@
 import { FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { FormSelectProps } from "@/types/RecipeForm.types"
+import { CategoriesProps, FormSelectProps } from "@/types/RecipeForm.types"
 
-export const FormSelect = ({ name, placeholder, control, label }: FormSelectProps) => {
+export const FormSelect = ({ name, placeholder, control, label, categories }: FormSelectProps) => {
   return (
-    <div className="flex flex-col gap-1">
-      <FormField
-        control={control}
-        name="category"
-        render={({ field }) => (
-          <FormItem id={name}>
-            <FormLabel>{label}</FormLabel>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
-              <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder={placeholder} />
-                </SelectTrigger>
-              </FormControl>
-              <SelectContent>
-                <SelectItem value="Makanan_Berat">Makanan Berat</SelectItem>
-                <SelectItem value="Makanan_Ringan">Makanan Ringan</SelectItem>
-                <SelectItem value="Kue">Kue</SelectItem>
-              </SelectContent>
-            </Select>
-          </FormItem>
-        )}
-      />
-    </div>
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem id={name}>
+          <FormLabel>{label}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {categories.map((category: CategoriesProps, index: number) => (
+                <SelectItem key={index} value={category.value}>
+                  {category.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </FormItem>
+      )}
+    />
   )
 }
