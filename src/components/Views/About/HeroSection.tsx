17d3bcf6b7afbe7f2eec18c167bde25dc79419c2
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:03:47 +0700

    style: update HeroSection component to support dark mode text color for subheading

--- src/components/Views/About/HeroSection.tsx
+++ src/components/Views/About/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSectionAbout = ({ heading, subheading, src }: HeroSectionAboutProps) =
     <div className="mt-24 lg:mt-28">
       <div className="mx-auto flex flex-col items-center justify-center lg:w-4/5 lg:text-center">
         <h1 className="heading font-semibold lg:w-2/3">{heading}</h1>
-        <p className="subheading mt-5 font-light text-gray-500 lg:w-4/5">{subheading}</p>
+        <p className="subheading mt-5 text-gray-500 dark:text-white lg:w-4/5">{subheading}</p>
       </div>
       <div className="mt-10 lg:py-28">
         <img src={src} alt="bg-about" className="w-full rounded-xl" />
