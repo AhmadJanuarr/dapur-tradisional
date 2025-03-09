0266d898d2065119a92afcdb9560bf3653a4805d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:58:58 +0700

    style: improve FormFieldContact component for better dark mode support and visual consistency

--- src/components/FormField/FormFieldContact.tsx
+++ src/components/FormField/FormFieldContact.tsx
@@ -16,11 +16,13 @@ type NameFieldProps = {
 const FieldInput = ({ icon, placeholder, type }: FieldProps) => {
   return (
     <div className="relative gap-2">
-      {icon && <span className="absolute left-7 top-1/2 -translate-y-1/2 transform text-gray-500">{icon}</span>}
+      {icon && (
+        <span className="absolute left-7 top-1/2 -translate-y-1/2 transform text-gray-500 dark:text-white">{icon}</span>
+      )}
       <Input
         placeholder={placeholder}
         type={type}
-        className={`rounded-full px-6 py-6 text-gray-500 placeholder:text-[0.8rem] ${icon ? "pl-16" : ""} shadow-none`}
+        className={`px-6 py-6 text-gray-500 placeholder:text-[1rem] dark:text-white dark:placeholder:text-white ${icon ? "pl-16" : ""} shadow-none`}
       ></Input>
     </div>
   )
@@ -33,7 +35,7 @@ const NameFieldInput = ({ placeholder }: NameFieldProps) => {
         <Input
           key={index}
           placeholder={item}
-          className="rounded-full px-6 py-6 text-gray-500 shadow-none placeholder:text-[0.8rem]"
+          className="px-6 py-6 text-gray-500 shadow-none placeholder:text-[1rem] dark:text-white dark:placeholder:text-white "
         />
       ))}
     </div>
@@ -43,16 +45,16 @@ const NameFieldInput = ({ placeholder }: NameFieldProps) => {
 const FormFieldContact = () => {
   return (
     <form>
-      <div className="subheading flex w-full flex-col gap-3 font-inter">
+      <div className="subheading font-inter flex w-full flex-col gap-3">
         <NameFieldInput placeholder={["Nama Depan", "Nama Belakang"]} />
         <FieldInput icon={<Mail size={20} />} placeholder="Email" type="email" />
         <FieldInput icon={<Contact size={20} />} placeholder="No Handphone" type="text" />
         <Textarea
           placeholder="Bagaimana kami bisa membantu?"
           rows={5}
-          className="rounded-xl px-3 py-5 font-inter text-gray-500 placeholder:text-[0.8rem]"
+          className="font-inter rounded-md px-3 py-5 text-gray-500 placeholder:text-[1rem] dark:text-white dark:placeholder:text-white"
         />
-        <Button type="submit" variant="default" className="py-5">
+        <Button type="submit" variant="default" className="rounded-md py-5">
           Submit
         </Button>
       </div>
