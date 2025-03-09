dfa52507a7602fbb15b5b6bec7a7ced995b38e31
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:09:04 +0700

    refactor: enhance type definitions in RecipeForm.types.ts for better clarity and structure

--- src/types/RecipeForm.types.ts
+++ src/types/RecipeForm.types.ts
@@ -1,43 +1,61 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Control, FieldArrayWithId, UseFormRegister } from "react-hook-form";
-
+import { Control, FieldArrayWithId, UseFormRegister } from "react-hook-form"
 
 export interface FormInputProps {
-    name: "title" | "description"
-    label: string
-    placeholder: string
-    type: string
-    isTextArea?: boolean
-    control: Control<RecipeFormValues>
+  name: keyof RecipeFormValues
+  label: string
+  placeholder: string
+  type: string
+  isTextArea?: boolean
+  control: Control<RecipeFormValues>
+}
+export interface CategoriesProps {
+  value: string
+  label: string
 }
 
 export interface FormSelectProps {
-    name: string
-    label: string
-    placeholder: string
-    control: Control<RecipeFormValues>
+  name: "difficulty" | "category"
+  label: string
+  placeholder: string
+  control: Control<RecipeFormValues>
+  categories: { value: string; label: string }[]
 }
 
 export interface FormImageProps {
-    name: string
-    label: string
-    type: string
-    control: Control<RecipeFormValues>
+  name: string
+  label: string
+  type: string
+  control: Control<RecipeFormValues>
 }
 
 export interface FormArrayProps {
-    title: string
-    prefix: string
-    fields: FieldArrayWithId<any, string>[]
-    onAdd: (e: React.BaseSyntheticEvent) => void
-    onRemove: (index: number) => void
-    register: UseFormRegister<RecipeFormValues>;
+  title: string
+  prefix: string
+  fields: FieldArrayWithId<any, string>[]
+  onAdd: (e: React.BaseSyntheticEvent) => void
+  onRemove: (index: number) => void
+  register: UseFormRegister<RecipeFormValues>
 }
 export interface RecipeFormValues {
-    title: string;
-    description: string;
-    image: string;
-    category: string;
-    ingredients: string[];
-    steps: string[];
-  }
+  title: string
+  description: string
+  image: string
+  category: string
+  ingredients: string[]
+  steps: string[]
+  calories: number | null
+  protein: number | null
+  carbs: number | null
+  fat: number | null
+  difficulty: string
+  tips: string
+}
+
+export interface PagintaionControlProps {
+  current: number
+  numbers: number[]
+  prevPage: () => void
+  changePage: (e: number) => void
+  nextPage: () => void
+}
