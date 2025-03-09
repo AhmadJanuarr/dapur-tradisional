e1671daffdfba468a429ac646b91b0e2c7a5b120
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:45:18 +0700

    fix: ensure controlled input values in FormInput component for consistent behavior

--- src/components/Admin/Form/FormInput.tsx
+++ src/components/Admin/Form/FormInput.tsx
@@ -5,23 +5,28 @@ import { FormInputProps } from "@/types/RecipeForm.types"
 
 export const FormInput = ({ name, label, placeholder, control, type = "text", isTextArea = false }: FormInputProps) => {
   return (
-    <div className="grid w-full items-center gap-1.5">
-      <FormField
-        control={control}
-        name={name}
-        render={({ field }) => (
-          <FormItem id={name}>
-            <FormLabel>{label}</FormLabel>
-            <FormControl>
-              {isTextArea ? (
-                <Textarea id={name} placeholder={placeholder} {...field} />
-              ) : (
-                <Input type={type} id={name} placeholder={placeholder} {...field} />
-              )}
-            </FormControl>
-          </FormItem>
-        )}
-      />
-    </div>
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem id={name}>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            {isTextArea ? (
+              <Textarea id={name} placeholder={placeholder} {...field} value={field.value ?? ""} />
+            ) : (
+              <Input
+                type={type}
+                id={name}
+                placeholder={placeholder}
+                {...field}
+                value={field.value ?? ""}
+                className="subheading"
+              />
+            )}
+          </FormControl>
+        </FormItem>
+      )}
+    />
   )
 }
