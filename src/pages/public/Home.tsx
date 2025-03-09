9c11a72f5888ad8aea6f0aafd65e75d85341ec72
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:08:30 +0700

    feat: update HomePage component with CallToAction and fix image path

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,14 +1,15 @@
+import { Fragment } from "react/jsx-runtime"
 import HeroSectionHome from "@/components/Views/Home/HeroSection"
 import TraditionalCuisineBenefits from "@/components/Views/Home/TraditionalCuisineBenefits"
 import RecipeStatistics from "@/components/Views/Home/RecipeStatistics"
 import SupportFaq from "@/components/Views/Home/SupportFaq"
-import { Fragment } from "react/jsx-runtime"
+import CallToAction from "@/components/Views/Home/CallToAction"
 
 export default function HomePage() {
   return (
     <Fragment>
       <HeroSectionHome
-        heading="Masak Dengan Lebih Mudah & Kreatif."
+        heading="Masak dengan lebih mudah & kreatif."
         description="Dengan Dapur Tradisional, Anda bisa menemukan inspirasi untuk masakan lezat setiap hari. Cari resep favorit Anda sekarang!"
         callToAction="Jelajahi Resep Terbaru"
         to="/recipes"
@@ -17,9 +18,10 @@ export default function HomePage() {
       <TraditionalCuisineBenefits heading="Mengapa Masakan Tradisional Tak Tergantikan?" />
       <RecipeStatistics
         heading="Daftar resep yang tersedia sekarang"
-        img="/img/makanan-arrival.png"
+        img="/elements/makanan-arrival.png"
         description="Dapur Tradisional menyediakan berbagai macam resep makanan ringan khas daerah yang penuh cita rasa dan keunikan. Dengan resep-resep ini, Anda dapat menikmati kelezatan kuliner khas daerah dengan mudah."
       />
+      <CallToAction />
       <SupportFaq
         heading="Frequently Asked Questions"
         subheading="Jawaban untuk Pertanyaan Umum tentang Resep dan Penggunaan Website"
