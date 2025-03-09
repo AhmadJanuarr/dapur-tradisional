b5d2112292c5b5c99a38fec6ade9478c97a840df
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:43:51 +0700

    feat: add delete functionality with confirmation dialog to RecipeList component

--- src/components/Admin/RecipeList.tsx
+++ src/components/Admin/RecipeList.tsx
@@ -1,6 +1,19 @@
 import { TableBody, TableCell, TableRow } from "@/components/ui/table"
 import { EditIcon, Trash2 } from "lucide-react"
 import { Button } from "../ui/button"
+import axios from "axios"
+import { toast } from "sonner"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog"
 
 interface Recipe {
   title: string
@@ -8,49 +21,61 @@ interface Recipe {
   image: string
   description: string
   category: string
-  ingredients: { name: string; id: number }[]
-  steps: { name: string; id: number }[]
+  difficulty: string
 }
-export default function RecipeList({ recipes }: { recipes: Recipe[] }) {
+
+export default function RecipeList({ recipes = [] }: { recipes: Recipe[] }) {
+  const handleDelete = async (id: number) => {
+    try {
+      const response = await axios.delete(`${import.meta.env.VITE_API_URL}/api/recipes/${id}`)
+      toast.success(response.data.message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <TableBody>
-      {recipes.slice(0, 10).map(({ title, id, image, description, category, ingredients, steps }: Recipe) => (
-        <TableRow key={title} className="text-[13px]">
-          <TableCell className="font-medium ">{id}</TableCell>
-          <TableCell>{title}</TableCell>
-          <TableCell>
-            <img
-              src={`${import.meta.env.VITE_API_URL}/images/${image}`}
-              width={100}
-              height={100}
-              alt="image"
-              loading="lazy"
-            />
+      {recipes.slice(0, 10).map(({ title, image, description, category, difficulty, id }: Recipe, index) => (
+        <TableRow key={title} className="subheading">
+          <TableCell className="text-center font-medium">{index + 1}</TableCell>
+          <TableCell className="flex items-center gap-2">
+            <img src={image} width={100} alt="image" loading="lazy" className="rounded-sm" />
+            {title}
           </TableCell>
-          <TableCell>{description}</TableCell>
+          <TableCell>{description.slice(0, 100)}...</TableCell>
           <TableCell>{category.replace("_", " ")}</TableCell>
           <TableCell>
-            <ol className="list-inside list-disc">
-              {ingredients.map(({ name, id }: { name: string; id: number }) => (
-                <li key={id}>{name}</li>
-              ))}
-            </ol>
+            <div
+              className={`${difficulty === "Mudah" ? "bg-green-200  text-green-900" : difficulty === "Sedang" ? "bg-yellow-200 text-yellow-900" : "bg-red-200 text-red-900"} rounded-md py-1 text-center`}
+            >
+              {difficulty}
+            </div>
           </TableCell>
           <TableCell>
-            <ol className="list-inside list-disc">
-              {steps.map(({ name, id }: { name: string; id: number }) => (
-                <li key={id}>{name}</li>
-              ))}
-            </ol>
-          </TableCell>
-          <TableCell>
-            <div className="flex justify-center gap-2">
-              <Button className="btn btn-primary btn-sm" variant="default">
+            <div className="flex justify-center gap-5">
+              <Button variant="link" className="p-0">
                 <EditIcon />
               </Button>
-              <Button className="btn btn-error btn-sm" variant="destructive">
-                <Trash2 />
-              </Button>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="link" className="p-0 text-red-500">
+                    <Trash2 />
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Apakah kamu yakin ingin menghapus ini ?</AlertDialogTitle>
+                    <AlertDialogDescription>Kamu akan menghapus resep ini secara permanen.</AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel className="rounded-md">Batalkan</AlertDialogCancel>
+                    <AlertDialogAction onClick={() => handleDelete(id)} className="rounded-md bg-red-500">
+                      Lanjutkan
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </div>
           </TableCell>
         </TableRow>
