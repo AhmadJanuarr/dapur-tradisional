1f068a34f28feedcf89daf4f341b693f3c6c46d5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:00:26 +0700

    feat: add profile route and enhance layout responsiveness with updated styles

--- src/components/Layout/App/index.tsx
+++ src/components/Layout/App/index.tsx
@@ -9,21 +9,23 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   const pathname = location.pathname
 
   const routes = {
-    availablePaths: ["/", "/about", "/contact", "/recipes"],
+    availablePaths: ["/", "/about", "/contact", "/recipes", "/profile"],
     isAdminRoute: location.pathname.startsWith("/admin"),
     isFullWidthRoute: ["/auth/login", "/auth/register", "/contact"].includes(location.pathname),
     isHome: pathname === "/",
   }
   const showHeaderFooter = routes.availablePaths.includes(pathname) || pathname.startsWith("/recipes/")
   const mainWidth =
-    routes.isAdminRoute || routes.isFullWidthRoute || routes.isHome ? "w-full" : "lg:w-4/5 w-full px-5 lg:px-0"
+    routes.isAdminRoute || routes.isFullWidthRoute || routes.isHome
+      ? "w-full"
+      : "2xl:w-4/5 lg:w-11/12 w-full px-5 lg:px-0"
 
   return (
-    <div className="flex w-full flex-col items-center justify-center font-inter">
+    <div className="flex w-full flex-col items-center justify-center font-rubik transition-all duration-300">
       <div className="w-full">
         {showHeaderFooter && <Header />}
         <Toaster richColors />
-        <main className="flex w-full items-center justify-center">
+        <main className="flex w-full items-center justify-center dark:bg-darkPrimary ">
           <div className={mainWidth}>{children}</div>
         </main>
         {showHeaderFooter && <Footer />}
