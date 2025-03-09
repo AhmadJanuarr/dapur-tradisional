442e21b39d0f55ee58397cc1edd314e512dd29ee
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 05:45:03 +0700

    refactor: simplify structure of FormImage component for improved readability

--- src/components/Admin/Form/FormImage.tsx
+++ src/components/Admin/Form/FormImage.tsx
@@ -4,28 +4,26 @@ import { FormImageProps } from "@/types/RecipeForm.types"
 
 export const FormImage = ({ name, control, label, type }: FormImageProps) => {
   return (
-    <div className="flex flex-col gap-1">
-      <FormField
-        control={control}
-        name="image"
-        render={({ field: { value, ...fieldProps } }) => (
-          <FormItem id={name}>
-            <FormLabel>{label}</FormLabel>
-            <FormControl>
-              <Input
-                {...fieldProps}
-                id={name}
-                type={type}
-                accept="image/*"
-                onChange={(event) => {
-                  const file = event.target.files?.[0]
-                  fieldProps.onChange(file)
-                }}
-              />
-            </FormControl>
-          </FormItem>
-        )}
-      />
-    </div>
+    <FormField
+      control={control}
+      name="image"
+      render={({ field: { value, ...fieldProps } }) => (
+        <FormItem id={name}>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              {...fieldProps}
+              id={name}
+              type={type}
+              accept="image/*"
+              onChange={(event) => {
+                const file = event.target.files?.[0]
+                fieldProps.onChange(file)
+              }}
+            />
+          </FormControl>
+        </FormItem>
+      )}
+    />
   )
 }
