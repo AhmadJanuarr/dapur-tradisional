f665c400f09be665431e3097eed71a8beed50eba
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:02:03 +0700

    feat: enhance Header component with user authentication and theme toggle functionality

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,16 +1,29 @@
-import { Link, useLocation, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react"
-import { AlignJustify, HeartIcon } from "lucide-react"
+import { AlignJustify, Bookmark, LogOut, Moon, Settings, Sun, User } from "lucide-react"
 import { LIST_MENU } from "@/data/datas"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Separator } from "@/components/ui/separator"
+import { LoadingFullScreen } from "@/components/Loading"
+import { useTheme } from "@/hooks/useTheme"
+import { useHeader } from "@/hooks/useHeader"
 
 type NavLinkProps = {
   children: React.ReactNode
   href: string
   onClick?: () => void
+}
+type UserAlreadyLoggedProps = {
+  theme: string
+  handleAuth: (action: string) => void
+  toggleDarkMode: () => void
+  handleUserProfile?: () => void | undefined
+}
+
+interface UserNotLooggedProps extends UserAlreadyLoggedProps {
+  isHome: boolean
+  isScrollY: boolean
 }
 
 const NavLink = ({ children, href, onClick }: NavLinkProps) => {
@@ -18,59 +31,107 @@ const NavLink = ({ children, href, onClick }: NavLinkProps) => {
     <li>
       <Link to={href} className="hover:underline" onClick={onClick}>
         {children}
-        <hr className="mt-2 border-gray-200" />
       </Link>
+      <hr className="mt-2 block border-gray-200 md:hidden" />
     </li>
   )
 }
 
-const LogoHeader = ({ title }: { title: string }) => {
+const LogoHeader = () => {
   return (
-    <Link to="/" className="flex items-center gap-2 font-inter text-xl md:text-2xl">
+    <Link to="/" className="flex w-1/3 items-center gap-2 text-xl md:text-2xl">
       <img src="/logo/logo-t.png" alt="logo website" className="w-10" />
-      {title}
+      Dapur <span className="text-[#F97316]">tadisional</span>
     </Link>
   )
 }
 
-export default function Header() {
-  const [open, setOpen] = useState<boolean>(false)
-  const [isScrollY, setIsScrollY] = useState<boolean>(false)
-  const navigate = useNavigate()
-  const location = useLocation()
-  const isHome = location.pathname === "/"
-  const isLogin = typeof window !== "undefined" && Boolean(localStorage.getItem("token"))
+const UserAlreadyLogged = ({ handleAuth, toggleDarkMode, theme, handleUserProfile }: UserAlreadyLoggedProps) => (
+  <div className="hidden w-1/3 items-center justify-end gap-5 md:flex">
+    {theme === "dark" ? (
+      <Sun onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer" />
+    ) : (
+      <Moon onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer" />
+    )}
+    <Separator orientation="vertical" className="h-5 bg-gray-700" />
+    <Bookmark className="h-5 w-5 cursor-pointer" />
+    <Popover>
+      <PopoverTrigger>
+        <Avatar>
+          <AvatarImage src="/img/user.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent side="top" align="end" className="z-50">
+        <div className="grid gap-4">
+          <ul className="subheading space-y-4">
+            <li className="flex items-center gap-2">
+              <img src="/img/user.png" alt="user" className="h-12 w-12 rounded-full" />
+              <div className="flex items-center gap-2">Username</div>
+            </li>
+            <li
+              className="flex cursor-pointer items-center gap-5 hover:underline"
+              onClick={() => handleUserProfile?.()}
+            >
+              <User className="h-5 w-5" /> Profil Saya
+            </li>
+            <li className="flex cursor-pointer items-center gap-5 hover:underline">
+              <Settings className="h-5 w-5" />
+              Pengaturan
+            </li>
+            <Separator />
+            <li onClick={() => handleAuth("keluar")} className="flex cursor-pointer items-center gap-5 hover:underline">
+              <LogOut className="h-5 w-5" />
+              Keluar
+            </li>
+          </ul>
+        </div>
+      </PopoverContent>
+    </Popover>
+  </div>
+)
 
-  const handleAuth = (value: string) => {
-    const routes: { [key: string]: string | (() => void) } = {
-      masuk: "/auth/login",
-      daftar: "/auth/register",
-      keluar: () => {
-        localStorage.removeItem("token")
-        navigate("/")
-      },
-    }
-    if (typeof routes[value] === "string") navigate(routes[value])
-    if (typeof routes[value] === "function") routes[value]()
-  }
-
-  useEffect(() => {
-    const handleScroll = () => setIsScrollY(window.scrollY > 100)
-    window.addEventListener("scroll", handleScroll)
-    window.scrollTo(0, 0)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [location])
-  return (
-    <header
-      className={`left-0 top-0 z-50 w-full  
-      ${isScrollY ? "fixed bg-white shadow-lg " : "absolute"}`}
+const UserNotLogged = ({ handleAuth, isHome, isScrollY, theme, toggleDarkMode }: UserNotLooggedProps) => (
+  <div className="hidden w-1/3 items-center justify-end gap-5 md:flex">
+    {theme === "dark" ? (
+      <Sun onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer" />
+    ) : (
+      <Moon onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer" />
+    )}
+    <Separator orientation="vertical" className="h-5 bg-gray-700" />
+    <Button
+      variant="ghost"
+      className={`${isHome && !isScrollY ? "border border-white" : "border border-black dark:border-white "} subheading`}
+      onClick={() => handleAuth("login")}
     >
+      Masuk
+    </Button>
+    <Button
+      variant={`${isHome && !isScrollY ? "secondary" : "default"}`}
+      onClick={() => handleAuth("register")}
+      className="subheading"
+    >
+      Daftar sekarang
+    </Button>
+  </div>
+)
+
+export default function Header() {
+  const { handleAuth, isHome, user, isScrollY, loading, open, setOpen } = useHeader()
+  const { toggleDarkMode, theme } = useTheme()
+  const navigate = useNavigate()
+
+  const handleUserProfile = () => {
+    navigate("/profile")
+  }
+  return (
+    <header className={`fixed left-0 top-0 z-30 w-full`}>
       <div
-        className={`z-50 flex justify-between  px-5 py-5 transition-all duration-300 ${isScrollY && isHome ? " bg-white text-black" : isHome ? "text-white" : "bg-white text-black"}`}
+        className={`flex w-full justify-between px-5 py-5 transition-all duration-200 ${isScrollY && isHome ? " bg-white text-black dark:bg-darkSecondary dark:text-white" : isHome ? "text-white" : "bg-white text-black dark:bg-darkSecondary dark:text-white"}`}
       >
-        <LogoHeader title="Dapur Tradisional" />
-        <nav className="hidden md:flex">
-          <ul className="flex items-center gap-5">
+        <LogoHeader />
+        <nav className="hidden w-1/3 justify-center md:flex">
+          <ul className="flex items-center justify-center gap-5">
             {LIST_MENU.usefulLinks.map((item) => (
               <NavLink href={item.href} key={item.name}>
                 {item.name}
@@ -78,55 +139,27 @@ export default function Header() {
             ))}
           </ul>
         </nav>
-        {/* Checking User Login */}
-        {isLogin ? (
-          <div className="hidden items-center gap-5 md:flex">
-            <HeartIcon className="h-5 w-5" />
-            <Popover>
-              <PopoverTrigger>
-                <Avatar>
-                  <AvatarImage src="/img/user.png" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </PopoverTrigger>
-              <PopoverContent side="top" align="end" className="w-auto text-[14px]">
-                <div className="grid gap-4">
-                  <ul className="space-y-2">
-                    <li>Profil Saya</li>
-                    <li>Pengaturan</li>
-                    <li>Mode Gelap</li>
-                    <Separator />
-                    <li onClick={() => handleAuth("keluar")} className="cursor-pointer hover:underline">
-                      Keluar
-                    </li>
-                  </ul>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </div>
+        {user.role === "USER" ? (
+          <UserAlreadyLogged
+            handleAuth={handleAuth}
+            toggleDarkMode={toggleDarkMode}
+            theme={theme}
+            handleUserProfile={handleUserProfile}
+          />
         ) : (
-          <div className="hidden gap-5 md:flex">
-            <Button
-              variant="ghost"
-              className={`${isHome && !isScrollY ? "border border-white" : "border border-black "} subheading`}
-              onClick={() => handleAuth("masuk")}
-            >
-              Masuk
-            </Button>
-            <Button
-              variant={`${isHome && !isScrollY ? "secondary" : "default"}`}
-              onClick={() => handleAuth("daftar")}
-              className="subheading"
-            >
-              Daftar sekarang
-            </Button>
-          </div>
+          <UserNotLogged
+            handleAuth={handleAuth}
+            isHome={isHome}
+            isScrollY={isScrollY}
+            theme={theme}
+            toggleDarkMode={toggleDarkMode}
+          />
         )}
 
         {/* Mobile Menu Toggle */}
-        <button className="flex items-center md:hidden" onClick={() => setOpen(!open)}>
+        <Button className="flex items-center md:hidden" onClick={() => setOpen(!open)}>
           <AlignJustify className="h-6 w-6" />
-        </button>
+        </Button>
       </div>
 
       {/* Mobile Devices */}
@@ -134,7 +167,10 @@ export default function Header() {
         className={`absolute left-0 -z-10  mx-auto w-full rounded-b-xl  
           shadow-lg transition-all duration-500 ${!open && "-translate-y-[30rem] opacity-0"}`}
       >
-        <div className="absolute -z-20 h-screen w-screen bg-slate-800 opacity-50 " onClick={() => setOpen(false)}></div>
+        <div
+          className="absolute -z-20 block h-screen w-screen bg-slate-800 opacity-50 md:hidden "
+          onClick={() => setOpen(false)}
+        ></div>
         <div className="rounded-b-xl border bg-white px-2 py-5">
           <ul className="flex w-full flex-col gap-5">
             {LIST_MENU.usefulLinks.map((item) => (
@@ -142,19 +178,19 @@ export default function Header() {
                 {item.name}
               </NavLink>
             ))}
-            {isLogin ? (
+            {user.role === "USER" ? (
               <li onClick={() => handleAuth("keluar")} className="cursor-pointer hover:underline">
                 Keluar
               </li>
             ) : (
               <div className="flex flex-col gap-5">
                 <li>
-                  <Button className="w-full rounded-xl" variant="secondary" onClick={() => handleAuth("masuk")}>
+                  <Button className="w-full rounded-xl" variant="secondary" onClick={() => handleAuth("login")}>
                     Masuk
                   </Button>
                 </li>
                 <li>
-                  <Button className="w-full rounded-xl" variant="default" onClick={() => handleAuth("daftar")}>
+                  <Button className="w-full rounded-xl" variant="default" onClick={() => handleAuth("signup")}>
                     Daftar sekarang
                   </Button>
                 </li>
@@ -163,6 +199,7 @@ export default function Header() {
           </ul>
         </div>
       </div>
+      {loading && <LoadingFullScreen />}
     </header>
   )
 }
