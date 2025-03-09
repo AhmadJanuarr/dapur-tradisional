8a6f4480cad13a25651cc840922701876057c1cc
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 06:02:09 +0700

    feat: update Loading component with improved styling and user feedback message

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,14 +1,11 @@
 export function LoadingFullScreen() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-100 dark:bg-gray-900 ">
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center gap-5 bg-gray-100 dark:bg-gray-900">
       <div
         className="text-surface inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
         role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
+      ></div>
+      <span className="animate-pulse text-xl">Tunggu sebentar sedang di proses</span>
     </div>
   )
 }
