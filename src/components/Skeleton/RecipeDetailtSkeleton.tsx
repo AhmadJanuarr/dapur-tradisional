f1c9b5a5604a5627b096e000194e424df06af2f0
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 23:57:54 +0700

    fix: adjust width of RecipeDetailtSkeleton for improved responsiveness

--- src/components/Skeleton/RecipeDetailtSkeleton.tsx
+++ src/components/Skeleton/RecipeDetailtSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "../ui/skeleton"
 
 export default function RecipeDetailtSkeleton() {
   return (
-    <div className="mx-auto mt-28 flex w-[80%] flex-col flex-wrap">
+    <div className="mx-auto mt-28 flex flex-col flex-wrap md:w-[80%]">
       <Skeleton className="mb-2 h-[50px] w-full rounded-xl" />
       <Skeleton className="mb-5 h-[20px] w-full rounded-xl" />
       <Skeleton className="mb-2 h-[600px] w-full rounded-xl" />
