a5ac93e933cc4d242546df240a871e0678a3e399
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-10 23:58:15 +0700

    fix: adjust padding in TraditionalCuisineBenefits component for improved layout

--- src/components/Views/Home/TraditionalCuisineBenefits.tsx
+++ src/components/Views/Home/TraditionalCuisineBenefits.tsx
@@ -9,13 +9,13 @@ interface TraditionalProps {
 
 const TraditionalCuisineBenefits = ({ heading }: { heading: string }) => {
   return (
-    <section className="mx-auto mt-10 flex flex-col items-center justify-center px-5 lg:mt-10 lg:w-11/12 lg:py-20 2xl:w-4/5">
+    <section className="mx-auto mt-10 flex flex-col items-center justify-center px-5 lg:mt-10 lg:w-11/12 2xl:w-4/5">
       <h1 className="heading w-full py-10 text-center font-semibold leading-tight lg:h-2/3 2xl:w-1/2 ">{heading}</h1>
       <div className="grid grid-cols-2 overflow-hidden rounded-2xl border bg-white shadow-none md:grid-cols-3">
         {ListTraditionalCuisineBenefits.map(({ title, description, icon: IconComponent }: TraditionalProps, index) => (
           <div
             key={title}
-            className={`flex flex-col justify-start border p-6 dark:border-gray-200 dark:bg-darkPrimary ${
+            className={`flex flex-col justify-start border p-2 dark:border-gray-200 dark:bg-darkPrimary lg:p-6 ${
               index < 3 ? "border-b" : ""
             } ${index % 3 !== 2 ? "md:border-r" : ""}`}
           >
