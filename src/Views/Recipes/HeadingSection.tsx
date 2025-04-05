c02ce7848e95f40110062d39a9612c110dafdb1d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:32:16 +0700

    feat: add HeadingSection component for Recipes page

---
+++ src/Views/Recipes/HeadingSection.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+
+export default function HeadingSection({
+  heading,
+  description,
+  isShowButton,
+  cta,
+}: {
+  heading: string
+  description?: string
+  isShowButton: boolean
+  cta?: string
+}) {
+  return (
+    <section className="w-full pb-8 pt-14 lg:pb-5 lg:pt-20">
+      <div className={`flex w-full ${isShowButton ? "justify-between" : " justify-center text-center"} `}>
+        <div>
+          <h1 className="heading font-prata">{heading}</h1>
+          <p className="subheading my-2 text-[#656565]">{description}</p>
+        </div>
+        {isShowButton && (
+          <Button variant="link" className="hidden w-1/5 items-center text-[16px] lg:flex">
+            {cta}
+            <ArrowRight />
+          </Button>
+        )}
+      </div>
+    </section>
+  )
+}
