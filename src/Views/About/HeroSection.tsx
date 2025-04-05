1c665a672d99c79b243cc491d78bc3d7203304e7
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:31:23 +0700

    feat: add HeroSectionAbout component for About page

---
+++ src/Views/About/HeroSection.tsx
@@ -0,0 +1,20 @@
+type HeroSectionAboutProps = {
+  heading: string
+  subheading: string
+  src: string
+}
+const HeroSectionAbout = ({ heading, subheading, src }: HeroSectionAboutProps) => {
+  return (
+    <div className="mt-24 lg:mt-40">
+      <div className="mx-auto flex flex-col items-center justify-center gap-8 text-center lg:w-4/5 lg:text-center">
+        <h1 className="heading font-prata lg:w-4/5">{heading}</h1>
+        <p className="subheading text-gray-500 dark:text-white lg:w-4/5">{subheading}</p>
+      </div>
+      <div className="mt-10 lg:py-10">
+        <img src={src} alt="bg-about" className=" h-[215px] w-full rounded-xl object-cover lg:h-[500px]" />
+      </div>
+    </div>
+  )
+}
+
+export default HeroSectionAbout
