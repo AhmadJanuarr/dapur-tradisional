82784833ea80f74c6c8b9885d5d98a51a2d9c1d6
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:31:27 +0700

    feat: add ProfileSection component for About page

---
+++ src/Views/About/ProfileSection.tsx
@@ -0,0 +1,29 @@
+type ProfileProps = {
+  name: string
+  role: string
+  src: string
+}
+
+const ProfileSection = ({ name, role, src }: ProfileProps) => {
+  return (
+    <section className="relative flex w-full">
+      <div className="w-full flex-col items-center justify-center rounded-[30px] bg-[#FFEDDF] py-8 lg:py-20">
+        <div className="flex flex-col items-center justify-center gap-5 text-center dark:text-white">
+          <h1 className="text-xl font-semibold lg:text-[40px]">Meet Our Team</h1>
+          <p className="subheading py-3 text-gray-500">
+            Individu-individu berbakat di balik proyek-proyek kami yang sukses
+          </p>
+        </div>
+        <figure>
+          <img src={src} alt={name} className="mx-auto w-[130px] rounded-full py-4 lg:w-[150px] lg:py-10" />
+          <figcaption className="subheading flex flex-col gap-3 text-center dark:text-white">
+            <h2 className="text-[0.9rem] lg:text-[24px]">{name}</h2>
+            <p className="subheading text-gray-500">{role}</p>
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+  )
+}
+
+export default ProfileSection
