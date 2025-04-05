dce9219b8b6ec890fb5e00de806b66867356c229
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:29:35 +0700

    feat: enhance TestimonialCard component with date and star rating features

--- src/components/Card/TestimonialCard.tsx
+++ src/components/Card/TestimonialCard.tsx
@@ -1,20 +1,29 @@
 type TestimonialCardProps = {
-  img: string
   name: string
-  role: string
   desc: string
+  date: string
+  stars: number
 }
-const TestimonialCard = ({ img, name, role, desc }: TestimonialCardProps) => {
+const TestimonialCard = ({ name, desc, date, stars }: TestimonialCardProps) => {
   return (
-    <div className="subheading dark:bg-darkBackground mr-5 flex w-[350px] flex-col gap-4 rounded-lg bg-slate-50 p-5 lg:h-[180px] lg:w-[500px]">
-      <div className="flex items-center gap-4">
-        <img src={img} alt={name} className="h-10 w-10 rounded-full" />
-        <div className="flex flex-col dark:text-white">
-          <h3 className="font-semibold">{name}</h3>
-          <p className="text-gray-500 dark:text-white">{role}</p>
-        </div>
+    <div className="subheading flex w-full flex-col justify-between gap-4 rounded-lg bg-[#F8F8F8] p-6 dark:bg-darkBackground lg:h-[280px]">
+      <div className="flex items-center gap-1">
+        {Array.from({ length: stars }).map((_, index) => (
+          <svg key={index} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#F97316" className="size-6">
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        ))}
       </div>
-      <p className="text-gray-500 dark:text-white">{desc}</p>
+
+      <p className="subheading text-[#434343] dark:text-white">{desc}</p>
+      <div className="flex flex-col dark:text-white">
+        <h3 className="font-semibold">{name}</h3>
+        <p className="text-gray-500 dark:text-white">{date}</p>
+      </div>
     </div>
   )
 }
