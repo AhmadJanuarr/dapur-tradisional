b6244dc39c571c2796d34f477c55d6a97badea55
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:29:27 +0700

    fix: update ProcessCard styles for improved layout and typography

--- src/components/Card/ProcessCard.tsx
+++ src/components/Card/ProcessCard.tsx
@@ -5,11 +5,11 @@ type ProcessCardProps = {
 }
 const ProcessCard = ({ index, title, desc }: ProcessCardProps) => {
   return (
-    <div className="subheading h relative flex h-[150px] w-full flex-col gap-4 rounded-xl border p-5 pt-5 dark:border-gray-50 md:h-[250px] lg:h-[220px] lg:w-1/4">
-      <div className="bg-darkPrimary absolute -top-3 left-5 rounded-full px-2 text-white dark:bg-white dark:text-black">
+    <div className="subheading relative flex h-[180px] w-full flex-col gap-5 rounded-xl border p-5 pt-6 dark:border-gray-50 md:h-[250px] lg:h-[220px]">
+      <div className="absolute -top-3 left-5 rounded-full bg-darkPrimary px-2 text-white dark:bg-white dark:text-black">
         {index}
       </div>
-      <h1 className="font-semibold">{title}</h1>
+      <h1 className="font-prata text-[24px]">{title}</h1>
       <p className="text-gray-500 dark:text-white">{desc}</p>
     </div>
   )
