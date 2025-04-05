5b09128f51243d1a96579c7d53997e7b7795e1c1
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:30:53 +0700

    feat: enhance ContactPage with heading and subheading in HeroSectionContact

--- src/pages/public/Contact.tsx
+++ src/pages/public/Contact.tsx
@@ -1,10 +1,13 @@
-import HeroSectionContact from "@/components/Views/Contact/HeroSection"
+import HeroSectionContact from "@/Views/Contact/HeroSection"
 import { Fragment } from "react/jsx-runtime"
 
 export default function ContactPage() {
   return (
     <Fragment>
-      <HeroSectionContact />
+      <HeroSectionContact
+        heading="Hubungi Kami"
+        subheading="Kami siap membantu Anda dalam mewujudkan ide dan proyek digital yang Anda impikan! Jika Anda memiliki pertanyaan, ingin berdiskusi lebih lanjut, atau membutuhkan solusi khusus untuk bisnis Anda"
+      />
     </Fragment>
   )
 }
