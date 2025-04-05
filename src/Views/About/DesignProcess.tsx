b32d6e49476c7fb2ee8d933cde530fc6a78a4de0
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-05 10:31:15 +0700

    feat: add DesignProcess component with HeadingSection and ProcessCard

---
+++ src/Views/About/DesignProcess.tsx
@@ -0,0 +1,21 @@
+import ProcessCard from "@/components/Card/ProcessCard"
+import HeadingSection from "./HeadingSection"
+import { DesignData } from "@/data/design.data"
+
+const DesignProcess = () => {
+  return (
+    <section className="py-10 pt-20 lg:pt-40">
+      <HeadingSection
+        heading="Our Process"
+        description="Pendekatan sistematis untuk menciptakan pengalaman digital yang luar biasa."
+      />
+      <div className="grid gap-8 py-10 lg:grid-cols-3 lg:gap-5">
+        {DesignData.map((data, index) => (
+          <ProcessCard key={data.id} index={index + 1} title={data.title} desc={data.desc} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default DesignProcess
