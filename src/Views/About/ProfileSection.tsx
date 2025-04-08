4003dc06b32a808162a9431cb29c1c23fabd07b1
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-08 10:03:13 +0700

    fix: update dark mode text color in ProfileSection component

--- src/Views/About/ProfileSection.tsx
+++ src/Views/About/ProfileSection.tsx
@@ -8,7 +8,7 @@ const ProfileSection = ({ name, role, src }: ProfileProps) => {
   return (
     <section className="relative flex w-full">
       <div className="w-full flex-col items-center justify-center rounded-[30px] bg-[#FFEDDF] py-8 lg:py-20">
-        <div className="flex flex-col items-center justify-center gap-5 text-center dark:text-white">
+        <div className="flex flex-col items-center justify-center gap-5 text-center dark:text-neutral-800">
           <h1 className="text-xl font-semibold lg:text-[40px]">Meet Our Team</h1>
           <p className="subheading py-3 text-gray-500">
             Individu-individu berbakat di balik proyek-proyek kami yang sukses
@@ -16,7 +16,7 @@ const ProfileSection = ({ name, role, src }: ProfileProps) => {
         </div>
         <figure>
           <img src={src} alt={name} className="mx-auto w-[130px] rounded-full py-4 lg:w-[150px] lg:py-10" />
-          <figcaption className="subheading flex flex-col gap-3 text-center dark:text-white">
+          <figcaption className="subheading flex flex-col gap-3 text-center dark:text-neutral-800 ">
             <h2 className="text-[0.9rem] lg:text-[24px]">{name}</h2>
             <p className="subheading text-gray-500">{role}</p>
           </figcaption>
