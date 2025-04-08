6d02dea10034366955464dba8d861faa6d14873e
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-08 10:02:47 +0700

    fix: enhance dark mode styles in InspirationCard component

--- src/components/Card/InspirationCard.tsx
+++ src/components/Card/InspirationCard.tsx
@@ -19,10 +19,10 @@ const InspirationCard = ({ img, title, category, description, difficulty, onClic
           className="transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
         />
       </div>
-      <div className="rounded-b-lg border-x-2 border-b-2 border-slate-100 px-3 py-3 ">
+      <div className="rounded-b-lg border-x-2 border-b-2 border-slate-100 px-3 py-3 dark:bg-darkBackground">
         <p className="text-right text-[#f97316]">{category.replace("_", " ")}</p>
         <h3 className="font-semibold">{title}</h3>
-        <p className="py-2 text-[#656565] ">{description.slice(0, 100).concat("...")}</p>
+        <p className="py-2 text-[#656565] dark:text-slate-200">{description.slice(0, 100).concat("...")}</p>
         <div className="flex justify-between">
           <p>
             level : <span className="text-[#f97316]">{difficulty}</span>
