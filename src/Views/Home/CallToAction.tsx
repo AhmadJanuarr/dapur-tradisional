19c7d3f61388804f1f25e22df4e45b9cfb593b58
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:43:18 +0700

    feat: update CallToAction styles and text for improved layout and clarity

--- src/Views/Home/CallToAction.tsx
+++ src/Views/Home/CallToAction.tsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom"
 
 const CallToAction = () => {
   return (
-    <section className="relative mx-auto mt-24 flex flex-col items-center justify-center overflow-hidden bg-[#ffeddf] px-11 py-10 lg:mt-52 lg:h-[480px] lg:w-11/12 lg:flex-row lg:justify-start lg:rounded-3xl lg:px-20 lg:py-0 2xl:w-4/5">
+    <section className="relative mx-auto mt-16 flex flex-col items-center justify-center overflow-hidden  bg-[#ffeddf] px-11 py-10 lg:mt-12 lg:h-[480px] lg:w-11/12 lg:flex-row lg:justify-start lg:rounded-3xl lg:px-20 lg:py-0 2xl:w-4/5">
       <div className="z-20 flex flex-col gap-8 lg:w-3/5">
-        <h1 className="heading w-full font-prata leading-relaxed dark:text-black">
+        <h1 className="heading w-full font-raleway dark:text-black">
           Apakah kamu siap untuk memulai perjalanan kuliner baru?
         </h1>
         <div className="subheading flex items-center gap-5">
-          <Link to="/recipes">
+          <Link to="/resep">
             <Button className="rounded-full px-4 py-5 font-semibold lg:py-6">
               <Dessert />
               Jelajahi resep terbaru
