462c7dd8710b7b28e8248d1596193eec215cd3b3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:24:29 +0700

    fix: update BookMarksButton to use Heroicons and handle favorite state

--- src/components/Button/Bookmark.tsx
+++ src/components/Button/Bookmark.tsx
@@ -1,7 +1,19 @@
-import { Bookmark } from "lucide-react"
+import { BookmarkIcon } from "@heroicons/react/24/outline"
+import { BookmarkIcon as BookmarkIconSolid } from "@heroicons/react/24/solid"
+type BookMarksButtonProps = {
+  isFavorite?: boolean
+  onClickFavorite: () => void
+}
 
-export const BookMarksButton = () => (
-  <div className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow">
-    <Bookmark className="text-[#f97316]" size={25} />
+export const BookMarksButton = ({ onClickFavorite, isFavorite }: BookMarksButtonProps) => (
+  <div
+    onClick={onClickFavorite}
+    className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow"
+  >
+    {!isFavorite ? (
+      <BookmarkIcon className="h-6 w-6 text-[#f97316]" />
+    ) : (
+      <BookmarkIconSolid className="h-6 w-6 text-[#f97316]" />
+    )}
   </div>
 )
