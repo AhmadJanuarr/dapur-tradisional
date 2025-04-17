98c65968315a2054ebe8129149c5d3434d0704c1
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:25:47 +0700

    feat: enhance AuthContext with user profile update functionality and role-based access management

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from "axios"
-import { useState, ReactNode, createContext, useReducer, useContext } from "react"
+import { formSchemaLogin, formSchemaRegister } from "@/schemas/FormSchema"
 import { jwtDecode, JwtPayload } from "jwt-decode"
-import { useNavigate } from "react-router-dom"
+import { createContext, ReactNode, useContext, useEffect, useReducer } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
 import { toast } from "sonner"
 import { z } from "zod"
-import { formSchemaLogin, formSchemaRegister } from "@/schemas/FormSchema"
+import axios from "axios"
+import { useQueryClient } from "@tanstack/react-query"
+import { AxiosWithAuth } from "@/lib/AxiosWithAuth"
 
 interface AuthProviderProps {
   children: ReactNode
@@ -22,14 +24,22 @@ type RegisterProps = {
   email: string
   password: string
 }
+type User = {
+  id: number
+  name: string
+  email: string
+  role: string
+}
 
 interface AuthContextType {
-  user: any
+  user: User
   logout: () => void
   state: any
   login: (userData: LoginProps) => void
   signup: (userData: RegisterProps) => void
   dispatch: React.Dispatch<any>
+  updateName: (newName: string) => Promise<any>
+  updateEmail: (newEmail: string) => Promise<any>
 }
 interface JwtPayloadWithRole extends JwtPayload {
   role: string
@@ -42,6 +52,8 @@ const authReducer = (state: any, action: any) => {
       return { ...state, isLoading: true }
     case "STOP_LOADING":
       return { ...state, isLoading: false }
+    case "UPDATE_USER":
+      return { ...state, user: action.payload }
     default:
       return state
   }
@@ -49,24 +61,62 @@ const authReducer = (state: any, action: any) => {
 
 const AuthContext = createContext<AuthContextType>(null!)
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const queryClient = useQueryClient()
   const APIUrl = import.meta.env.VITE_API_URL
-  const [user, setUser] = useState({ role: "" })
   const [state, dispatch] = useReducer(authReducer, initialState)
+  const user = JSON.parse(localStorage.getItem("user") || "{}")
   const navigate = useNavigate()
+  const pathname = useLocation().pathname
+
+  useEffect(() => {
+    const isAdminRoute = pathname.startsWith("/admin")
+    const accessToken = localStorage.getItem("accessToken")
+
+    console.log("User Role:", user)
+    if (!isAdminRoute && user.role === "ADMIN") {
+      localStorage.removeItem("accessToken")
+      localStorage.removeItem("refreshToken")
+      localStorage.removeItem("user")
+    }
+
+    if (accessToken) {
+      try {
+        const expTime = new Date().getTime() / 1000
+        const decodedToken: JwtPayloadWithRole = jwtDecode(accessToken)
+        if (decodedToken.exp && decodedToken.exp < expTime) {
+          localStorage.removeItem("accessToken")
+          localStorage.removeItem("refreshToken")
+          localStorage.removeItem("user")
+          return
+        }
+      } catch (error) {
+        console.error("Error decoding token:", error)
+        localStorage.removeItem("accessToken")
+        localStorage.removeItem("refreshToken")
+        localStorage.removeItem("user")
+      }
+    }
+
+    if (user?.id) {
+      queryClient.invalidateQueries({ queryKey: ["recipes", user?.id] })
+    }
+  }, [pathname, queryClient, user, user?.id])
 
   const login = async (values: z.infer<typeof formSchemaLogin>) => {
     dispatch({ type: "START_LOADING" })
     try {
       const response = await axios.post(`${APIUrl}/api/auth/login`, values)
       const data = await response.data
-
+      console.log(data.data)
       if (response.status === 200) {
         localStorage.setItem("accessToken", data.accessToken)
         localStorage.setItem("refreshToken", data.refreshToken)
+        localStorage.setItem("user", JSON.stringify(data.data))
+        queryClient.clear()
+        queryClient.invalidateQueries({ queryKey: ["recipes"] })
         const { accessToken } = await data
         const decodeToken: JwtPayloadWithRole = jwtDecode(accessToken)
 
-        setUser({ role: decodeToken.role })
         if (decodeToken.role === "ADMIN") {
           setTimeout(() => {
             navigate("/admin/dashboard")
@@ -99,7 +149,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
       }, 2500)
     }
   }
-  console.log(user)
 
   const signup = async (values: z.infer<typeof formSchemaRegister>) => {
     dispatch({ type: "START_LOADING" })
@@ -138,7 +187,52 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
         await axios.post(`${APIUrl}/api/auth/logout`, { refreshToken })
         localStorage.removeItem("accessToken")
         localStorage.removeItem("refreshToken")
-        setUser({ role: "" })
+        localStorage.removeItem("user")
+        queryClient.clear()
+        queryClient.invalidateQueries({ queryKey: ["recipes", user?.id] })
+        queryClient.removeQueries({ queryKey: ["recipes", user?.id] })
+        navigate("/")
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  const updateName = async (newName: string) => {
+    const accessToken = localStorage.getItem("accessToken")
+    const user = localStorage.getItem("user")
+
+    if (accessToken) {
+      try {
+        const data = JSON.parse(user!)
+        const response = await AxiosWithAuth.put(`${APIUrl}/api/auth/profile/update-name`, {
+          name: newName,
+        })
+        const updateUser = { ...data, name: newName }
+        localStorage.setItem("user", JSON.stringify(updateUser))
+        dispatch({ type: "UPDATE_NAME", payload: updateUser })
+        toast.success("Nama pengguna berhasil diperbarui")
+        return response
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  const updateEmail = async (newEmail: string) => {
+    const accessToken = localStorage.getItem("accessToken")
+    const user = localStorage.getItem("user")
+    if (accessToken) {
+      try {
+        const data = JSON.parse(user!)
+        const response = await AxiosWithAuth.put(`${APIUrl}/api/auth/profile/update-email`, {
+          email: newEmail,
+        })
+        const updateUser = { ...data, email: newEmail }
+        localStorage.setItem("user", JSON.stringify(updateUser))
+        dispatch({ type: "UPDATE_USER", payload: updateUser })
+        toast.success("Email pengguna berhasil diperbarui")
+        return response
       } catch (error) {
         console.log(error)
       }
@@ -146,7 +240,9 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   }
 
   return (
-    <AuthContext.Provider value={{ user, login, signup, logout, state, dispatch }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ updateEmail, updateName, login, user, signup, logout, state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
 
