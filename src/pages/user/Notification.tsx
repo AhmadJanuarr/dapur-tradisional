5bec9e5f1761c75fd5e3fd605897d953ab5eeee1
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:42:31 +0700

    feat: add Notification component with placeholder content

---
+++ src/pages/user/Notification.tsx
@@ -0,0 +1,7 @@
+export const Notification = () => {
+  return (
+    <div className="px-20">
+      <div className="heading flex min-h-screen items-center justify-center font-raleway">Coming soon</div>
+    </div>
+  )
+}
