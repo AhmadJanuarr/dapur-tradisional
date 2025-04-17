eb352a16ea08fc623c20b7d9f8013764d36cf059
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:43:14 +0700

    feat: enhance HeroSection styles for improved readability and layout

--- src/Views/Contact/HeroSection.tsx
+++ src/Views/Contact/HeroSection.tsx
@@ -5,8 +5,10 @@ import NewsLetter from "../Home/NewsLetter"
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-5">
-      <h5 className="subheading">Bicara dengan kami</h5>
-      <p className="text-[#656565]">Butuh respon cepat? Langsung chat dengan kami melalui sosial media dibawah ini</p>
+      <h5 className="text-[15px] lg:text-[18px]">Bicara dengan kami</h5>
+      <p className="subheading text-gray-800 dark:text-slate-400">
+        Butuh respon cepat? Langsung chat dengan kami melalui sosial media dibawah ini
+      </p>
       <div className="flex items-center gap-3">
         <FaDiscord className="size-5 text-[#F97316]" />
         <p className="underline">Discord</p>
@@ -19,8 +21,8 @@ const ContactInfo = () => {
         <FaTelegram className="size-5 text-[#F97316]" />
         <p className="underline">Telegram</p>
       </div>
-      <h5 className="subheading">Hubungi kami</h5>
-      <p className="text-[#656565]">Chat secara pribadi melalui whatsapp</p>
+      <h5 className="text-[15px] lg:text-[18px] ">Hubungi kami</h5>
+      <p className="text-gray-800 dark:text-slate-400">Chat secara pribadi melalui whatsapp</p>
       <div className="flex items-center gap-3">
         <FaWhatsapp className="size-5 text-[#F97316]" />
         <p className="underline">+62 812 3456 789</p>
@@ -39,7 +41,7 @@ const LeftFormCard = () => {
 
 const RightContactCard = () => {
   return (
-    <div className="flex flex-col pr-10 lg:w-[60%]">
+    <div className="flex flex-col pr-10 lg:w-[80%]">
       <ContactInfo />
     </div>
   )
@@ -50,8 +52,8 @@ const HeroSectionContact = ({ heading, subheading }: { heading: string; subheadi
     <section className="mt-24 flex w-full flex-col md:py-0 lg:mt-40">
       <div>
         <div className="mx-auto flex flex-col items-center justify-center gap-8 text-center lg:w-4/5 lg:text-center">
-          <h1 className="heading font-prata lg:w-4/5">{heading}</h1>
-          <p className="subheading text-gray-500 dark:text-white lg:w-4/5">{subheading}</p>
+          <h1 className="heading font-raleway lg:w-4/5">{heading}</h1>
+          <p className="subheading text-gray-900 dark:text-white lg:w-4/5">{subheading}</p>
         </div>
       </div>
       <div className="grid w-full grid-cols-1 gap-16 pt-16 lg:grid-cols-2">
