35b4c6bfb7c45a4e776990a289322916a074f18a
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:44:06 +0700

    feat: update TraditionalCuisineBenefits styles for improved typography and layout

--- src/Views/Home/TraditionalCuisineBenefits.tsx
+++ src/Views/Home/TraditionalCuisineBenefits.tsx
@@ -22,7 +22,9 @@ const TraditionalCuisineBenefits = ({ heading }: { heading: string }) => {
       />
 
       <div className="flex flex-col items-center justify-center lg:w-11/12 2xl:w-4/5">
-        <h1 className="heading w-full pb-5 text-center font-prata lg:h-2/3 lg:leading-relaxed 2xl:w-1/2">{heading}</h1>
+        <h1 className="heading w-full pb-5 text-center font-raleway lg:h-2/3 lg:leading-relaxed 2xl:w-1/2">
+          {heading}
+        </h1>
         <div className="grid grid-cols-1 overflow-hidden rounded-2xl border pt-11 shadow-none sm:grid-cols-2 md:grid-cols-3 lg:pt-0">
           {ListTraditionalCuisineBenefits.map(
             ({ title, description, icon: IconComponent }: TraditionalProps, index) => (
@@ -32,12 +34,12 @@ const TraditionalCuisineBenefits = ({ heading }: { heading: string }) => {
                   index < 3 ? "lg:border-b" : ""
                 } ${index % 3 !== 2 ? "lg:border-r" : ""}`}
               >
-                <div className="flex  items-center justify-center rounded-xl bg-[#ffeddf] p-4">
+                <div className="flex  justify-start rounded-xl bg-[#ffeddf] p-4 ">
                   <IconComponent className="size-9 text-[#f97316]" />
                 </div>
                 <div className="flex flex-col ">
                   <h2 className="subheading font-semibold text-black dark:text-white lg:my-6">{title}</h2>
-                  <p className="subheading mt-2 text-[#656565] dark:text-white">{description}</p>
+                  <p className="subheading mt-2 text-gray-800 dark:text-white">{description}</p>
                 </div>
               </div>
             ),
