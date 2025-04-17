58a559b8e984fa17a2a387bec424426e3edf855c
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:42:56 +0700

    feat: update HeadingSection styles with new font and color adjustments

--- src/Views/About/HeadingSection.tsx
+++ src/Views/About/HeadingSection.tsx
@@ -9,11 +9,11 @@ const HeadingSection = ({ heading, subheading, description }: HeadingSectionProp
     <div className="z-20 flex flex-col gap-5 py-8 dark:text-white lg:gap-8">
       <h1 className="text-xl tracking-widest lg:text-3xl">{heading.toUpperCase()}</h1>
       {subheading && (
-        <h2 className="font-prata text-[1.2rem] text-gray-700 dark:text-gray-300 lg:w-1/2 lg:text-[2.5rem]">
+        <h2 className="font-raleway text-[1.2rem] text-gray-700 dark:text-gray-300 lg:w-1/2 lg:text-[2.5rem]">
           {subheading}
         </h2>
       )}
-      <p className="subheading text-[#434343] dark:text-neutral-200 lg:w-1/2 lg:py-3">{description}</p>
+      <p className="subheading text-gray-800 dark:text-neutral-200 lg:w-1/2 lg:py-3">{description}</p>
     </div>
   )
 }
