3a8cc92685f76b3de86724337b6a19bd315d0753
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:43:05 +0700

    feat: update ProfileSection styles with improved text color for better readability

--- src/Views/About/ProfileSection.tsx
+++ src/Views/About/ProfileSection.tsx
@@ -10,7 +10,7 @@ const ProfileSection = ({ name, role, src }: ProfileProps) => {
       <div className="w-full flex-col items-center justify-center rounded-[30px] bg-[#FFEDDF] py-8 lg:py-20">
         <div className="flex flex-col items-center justify-center gap-5 text-center dark:text-neutral-800">
           <h1 className="text-xl font-semibold lg:text-[40px]">Meet Our Team</h1>
-          <p className="subheading py-3 text-gray-500">
+          <p className="subheading py-3 text-gray-800">
             Individu-individu berbakat di balik proyek-proyek kami yang sukses
           </p>
         </div>
@@ -18,7 +18,7 @@ const ProfileSection = ({ name, role, src }: ProfileProps) => {
           <img src={src} alt={name} className="mx-auto w-[130px] rounded-full py-4 lg:w-[150px] lg:py-10" />
           <figcaption className="subheading flex flex-col gap-3 text-center dark:text-neutral-800 ">
             <h2 className="text-[0.9rem] lg:text-[24px]">{name}</h2>
-            <p className="subheading text-gray-500">{role}</p>
+            <p className="subheading text-gray-800">{role}</p>
           </figcaption>
         </figure>
       </div>
