15246e5eb66904aaedde339f8e2181531e7dd093
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:25:34 +0700

    fix: enhance Header component with user profile handling and popover functionality

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator"
 import { LoadingFullScreen } from "@/components/Loading"
 import { useTheme } from "@/hooks/useTheme"
 import { useHeader } from "@/hooks/useHeader"
+import { useState } from "react"
 
 type NavLinkProps = {
   children: React.ReactNode
@@ -15,10 +16,11 @@ type NavLinkProps = {
   onClick?: () => void
 }
 type UserAlreadyLoggedProps = {
+  usernameUser?: string
   theme: string
   handleAuth: (action: string) => void
   toggleDarkMode: () => void
-  handleUserProfile?: () => void | undefined
+  handleUserProfile?: (target: string) => void | undefined
 }
 
 interface UserNotLooggedProps extends UserAlreadyLoggedProps {
@@ -41,55 +43,90 @@ const LogoHeader = () => {
   return (
     <Link to="/" className="flex w-1/3 items-center gap-2 text-[1rem] dark:text-white">
       <img src="/logo/logo-t.png" alt="logo website" className="w-10" />
-      Dapur <span className="text-[#F97316]">tadisional</span>
+      Dapur<span className="text-[#F97316]">tadisional</span>
     </Link>
   )
 }
 
-const UserAlreadyLogged = ({ handleAuth, toggleDarkMode, theme, handleUserProfile }: UserAlreadyLoggedProps) => (
-  <div className="hidden w-1/3 items-center justify-end gap-5 md:flex">
-    {theme === "dark" ? (
-      <Sun onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer text-white" />
-    ) : (
-      <Moon onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer" />
-    )}
-    <Separator orientation="vertical" className="h-5 bg-gray-700" />
-    <Bookmark className="h-5 w-5 cursor-pointer" />
-    <Popover>
-      <PopoverTrigger>
-        <Avatar>
-          <AvatarImage src="/img/user.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </PopoverTrigger>
-      <PopoverContent side="top" align="end" className="z-50">
-        <div className="grid gap-4">
-          <ul className="subheading space-y-4">
-            <li className="flex items-center gap-2">
-              <img src="/img/user.png" alt="user" className="h-12 w-12 rounded-full" />
-              <div className="flex items-center gap-2">Username</div>
-            </li>
-            <li
-              className="flex cursor-pointer items-center gap-5 hover:underline"
-              onClick={() => handleUserProfile?.()}
-            >
-              <User className="h-5 w-5" /> Profil Saya
-            </li>
-            <li className="flex cursor-pointer items-center gap-5 hover:underline">
-              <Settings className="h-5 w-5" />
-              Pengaturan
-            </li>
-            <Separator />
-            <li onClick={() => handleAuth("keluar")} className="flex cursor-pointer items-center gap-5 hover:underline">
-              <LogOut className="h-5 w-5" />
-              Keluar
-            </li>
-          </ul>
-        </div>
-      </PopoverContent>
-    </Popover>
-  </div>
-)
+const UserAlreadyLogged = ({
+  handleAuth,
+  toggleDarkMode,
+  theme,
+  handleUserProfile,
+  usernameUser,
+}: UserAlreadyLoggedProps) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+
+  const handlePopoverClose = () => {
+    setIsPopoverOpen(false) // Menutup popover
+  }
+  return (
+    <div className="hidden w-1/3 items-center justify-end gap-5 md:flex">
+      {theme === "dark" ? (
+        <Sun onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer text-white" />
+      ) : (
+        <Moon onClick={toggleDarkMode} className="h-5 w-5 cursor-pointer" />
+      )}
+      <Separator orientation="vertical" className="h-5 bg-gray-800" />
+      <Bookmark className="h-5 w-5 cursor-pointer dark:text-white" />
+      <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+        <PopoverTrigger>
+          <Avatar>
+            <AvatarImage src="/elements/element-user.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </PopoverTrigger>
+        <PopoverContent side="top" align="end" className="z-50">
+          <div className="subheading grid gap-4 font-mona tracking-wide">
+            <ul className="subheading space-y-4">
+              <li
+                className="flex cursor-pointer items-center gap-5 hover:underline"
+                onClick={() => {
+                  handleUserProfile?.("")
+                  handlePopoverClose()
+                }}
+              >
+                <img src="/elements/element-user.png" alt="user" className="h-12 w-12 rounded-full" />
+                <div className="subheading flex items-center gap-2 ">{usernameUser}</div>
+              </li>
+              <li
+                className="flex cursor-pointer items-center gap-5 hover:underline"
+                onClick={() => {
+                  handleUserProfile?.("")
+                  handlePopoverClose()
+                }}
+              >
+                <User className="h-5 w-5" />
+                Profil Saya
+              </li>
+              <li
+                className="flex cursor-pointer items-center gap-5 hover:underline"
+                onClick={() => {
+                  handleUserProfile?.("pengaturan-akun")
+                  handlePopoverClose()
+                }}
+              >
+                <Settings className="h-5 w-5" />
+                Pengaturan
+              </li>
+              <Separator />
+              <li
+                onClick={() => {
+                  handleAuth("keluar")
+                  handlePopoverClose()
+                }}
+                className="flex cursor-pointer items-center gap-5 hover:underline"
+              >
+                <LogOut className="h-5 w-5" />
+                Keluar
+              </li>
+            </ul>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
 
 const UserNotLogged = ({ handleAuth, theme, toggleDarkMode }: UserNotLooggedProps) => (
   <div className="hidden w-1/3 items-center justify-end gap-5 md:flex">
@@ -101,7 +138,7 @@ const UserNotLogged = ({ handleAuth, theme, toggleDarkMode }: UserNotLooggedProp
     <Separator orientation="vertical" className="h-5 bg-gray-700" />
     <Button
       variant="ghost"
-      className="subheading border border-black dark:border-white"
+      className="subheading border border-black dark:border-white dark:text-white"
       onClick={() => handleAuth("login")}
     >
       Masuk
@@ -117,8 +154,11 @@ export default function Header() {
   const { toggleDarkMode, theme } = useTheme()
   const navigate = useNavigate()
 
-  const handleUserProfile = () => {
-    navigate("/profile")
+  const handleUserProfile = (target: string) => {
+    const userName = user.name?.replace(" ", "-")
+    if (userName) {
+      navigate(`profile/${userName}/${target}`)
+    }
   }
   return (
     <header className={`fixed left-0 top-0 z-30 w-full`}>
@@ -141,6 +181,7 @@ export default function Header() {
             toggleDarkMode={toggleDarkMode}
             theme={theme}
             handleUserProfile={handleUserProfile}
+            usernameUser={user.name}
           />
         ) : (
           <UserNotLogged handleAuth={handleAuth} theme={theme} toggleDarkMode={toggleDarkMode} />
