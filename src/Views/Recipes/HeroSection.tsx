18638165a6e99f05ea11ef3d8a54d252022864ae
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 15:08:42 +0700

    feat: update HeroSection styles for improved typography and color contrast

--- src/Views/Recipes/HeroSection.tsx
+++ src/Views/Recipes/HeroSection.tsx
@@ -12,8 +12,8 @@ const HeroSectionRecipe = ({ heading, subheading, image }: HeroSectionProps) => 
     <section className="mt-10 flex w-full lg:mt-28">
       <div className="flex h-[500px] w-full flex-col-reverse gap-3 lg:flex-row ">
         <div className="flex w-full flex-col justify-center rounded-xl bg-[#ffeddf] px-4 py-4 text-black md:py-10 lg:w-[40%] lg:gap-5 lg:px-6 lg:py-32">
-          <h1 className="heading font-prata leading-tight">{heading}</h1>
-          <p className="subheading py-3 text-[#656565]">{subheading}</p>
+          <h1 className="heading font-raleway leading-tight">{heading}</h1>
+          <p className="subheading py-3 text-gray-800">{subheading}</p>
           <div className="relative py-5 lg:mt-3">
             <form className="flex w-full items-center justify-center gap-3">
               <Input
