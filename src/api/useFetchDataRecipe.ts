e582b80088c40bdbf767a04c607032054a51ed94
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:24:13 +0700

    fix: refactor fetchDataApi to fetch recipes with user ID and handle guest users

--- src/api/useFetchDataRecipe.ts
+++ src/api/useFetchDataRecipe.ts
@@ -1,17 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AxiosWithAuth } from "@/lib/AxiosWithAuth"
 import axios from "axios"
-
 const APIUrl = import.meta.env.VITE_API_URL
-export async function fetchDataApi() {
-  try {
-    const response = await axios.get(`${APIUrl}/api/recipes`)
-    const recipes = response.data?.data ?? response.data
-    if (Array.isArray(recipes)) {
-      return recipes
-    }
-    console.warn("Data tidak berbentuk array:", recipes)
-    return recipes
-  } catch (error) {
-    console.error("Gagal mengambil data:", error)
-    return []
+
+export async function fetchDataApiRecipes({ queryKey }: any) {
+  const [_, userId] = queryKey
+  const token = localStorage.getItem("accessToken")
+  const axiosInstance = token ? AxiosWithAuth : axios
+
+  const response = await axiosInstance.get(`${APIUrl}/api/recipes?uid=${userId ?? " "}`)
+  const recipes = response.data?.data ?? response.data
+
+  if (!token || userId === "guest") {
+    return recipes.map((r: any) => ({ ...r, isFavorite: false }))
   }
+
+  return recipes
 }
