f192ef582b0b327c866a30a9b6d2b3d84c1e9a79
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-17 14:24:20 +0700

    fix: update HeadingAuth component styles and logo size

--- src/components/Auth/HeadingAuth/index.tsx
+++ src/components/Auth/HeadingAuth/index.tsx
@@ -1,9 +1,9 @@
 export function HeadingAuth({ heading, description }: { heading: string; description: string }) {
   return (
     <div className="my-5 flex flex-col items-center justify-center text-center">
-      <img src="/logo/logo-t.png" alt="logo" className="w-14" />
-      <h2 className="text-3xl font-bold">{heading}</h2>
-      <p className="subheading-mobile mt-2 text-center md:w-2/3">{description}</p>
+      <img src="/logo/logo-t.png" alt="logo" className="w-16" />
+      <h2 className="font-raleway text-3xl font-bold">{heading}</h2>
+      <p className="subheading-mobile mt-2 text-center text-gray-800 dark:text-slate-400 md:w-2/3">{description}</p>
     </div>
   )
 }
