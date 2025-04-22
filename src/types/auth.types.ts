551f976c50af9c002dd1aa7571b5038e5b0def20
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 02:51:30 +0700

    feat: add authentication types and context props for user management

---
+++ src/types/auth.types.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export type User = {
+  id: number
+  name: string
+  email?: string
+  role?: string
+  avatar?: string
+}
+
+export interface AuthProviderProps {
+  children: React.ReactNode
+}
+
+export interface JwtPayloadWithRole {
+  exp: number
+  role: string
+}
+
+export type AuthContextProps = {
+  user: User | null
+  state: any
+  dispatch: React.Dispatch<any>
+  login: (data: { email: string; password: string }) => void
+  signup: (data: { name: string; email: string; password: string }) => void
+  logout: () => void
+  updateName: (newName: string) => void
+  updateEmail: (newEmail: string) => void
+  updatePassword: (newPass: string, currentPass: string) => void
+}
