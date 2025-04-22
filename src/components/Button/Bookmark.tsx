91948652ab6589a80d2c0dd30c46599357e19099
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 02:45:57 +0700

    feat: update BookMarksButton to use BookmarksButtonProps type

--- src/components/Button/Bookmark.tsx
+++ src/components/Button/Bookmark.tsx
@@ -1,11 +1,8 @@
+import { BookmarksButtonProps } from "@/types/components.type"
 import { BookmarkIcon } from "@heroicons/react/24/outline"
 import { BookmarkIcon as BookmarkIconSolid } from "@heroicons/react/24/solid"
-type BookMarksButtonProps = {
-  isFavorite?: boolean
-  onClickFavorite: () => void
-}
 
-export const BookMarksButton = ({ onClickFavorite, isFavorite }: BookMarksButtonProps) => (
+export const BookMarksButton = ({ isFavorite, onClickFavorite }: BookmarksButtonProps) => (
   <div
     onClick={onClickFavorite}
     className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow"
