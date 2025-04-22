d66c5865cf21240a50b779cb8aca51016d5d31ca
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 02:49:08 +0700

    feat: implement responsive sidebar and layout for AdminPanel

--- src/pages/admin/AdminPanel.tsx
+++ src/pages/admin/AdminPanel.tsx
@@ -1,15 +1,29 @@
-import { useLocation } from "react-router-dom"
-import DashboardPage from "./DashboardPage"
-import AppLayout from "@/components/Admin/AppLayout"
-import ManageRecipes from "./ManageRecipes"
-
+import { Outlet } from "react-router-dom"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { useEffect, useState } from "react"
+import { AppSideBar } from "@/components/Admin/SideBar"
 export default function AdminPanel() {
-  const location = useLocation().pathname
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
-    <AppLayout>
-      {location.includes("/admin/dashboard") && <DashboardPage />}
-      {location.includes("/admin/recipes") && <ManageRecipes />}
-      {location.includes("/admin/users") && <h1>Dashboard</h1>}
-    </AppLayout>
+    <SidebarProvider>
+      <AppSideBar />
+      <main className="w-full items-center justify-center bg-slate-50 dark:bg-darkPrimary">
+        {!isMobile && <SidebarTrigger className="block lg:hidden" />}
+        <header className="fixed z-50 flex w-full justify-between border-b-2 bg-white px-3 py-4 md:hidden">
+          <SidebarTrigger />
+          <h1 className="text-xl font-semibold">Admin Dashboard</h1>
+        </header>
+        <div className="pt-6 md:pt-0">
+          <Outlet />
+        </div>
+      </main>
+    </SidebarProvider>
   )
 }
