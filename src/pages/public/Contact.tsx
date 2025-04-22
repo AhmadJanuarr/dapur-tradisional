8b557f5b54dc87c2248ff3032609b4e5ab505ec0
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 02:49:56 +0700

    refactor: change Contact page imports to named exports and add NewsLetter component

--- src/pages/public/Contact.tsx
+++ src/pages/public/Contact.tsx
@@ -1,4 +1,5 @@
-import HeroSectionContact from "@/Views/Contact/HeroSection"
+import { HeroSectionContact } from "@/Views/Contact/HeroSection"
+import { NewsLetter } from "@/Views/Home/NewsLetter"
 import { Fragment } from "react/jsx-runtime"
 
 export default function ContactPage() {
@@ -8,6 +9,9 @@ export default function ContactPage() {
         heading="Hubungi Kami"
         subheading="Kami siap membantu Anda dalam mewujudkan ide dan proyek digital yang Anda impikan! Jika Anda memiliki pertanyaan, ingin berdiskusi lebih lanjut, atau membutuhkan solusi khusus untuk bisnis Anda"
       />
+      <div className="mt-20">
+        <NewsLetter />
+      </div>
     </Fragment>
   )
 }
