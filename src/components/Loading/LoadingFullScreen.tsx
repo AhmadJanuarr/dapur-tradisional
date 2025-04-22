4aa867a6b5b115212253be5ae5ec2f3e8c77f808
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 02:47:07 +0700

    feat: add LoadingFullScreen component for loading indication

---
+++ src/components/Loading/LoadingFullScreen.tsx
@@ -0,0 +1,11 @@
+export const LoadingFullScreen = () => {
+  return (
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center gap-5 bg-gray-100 dark:bg-gray-900">
+      <div
+        className="text-surface inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+        role="status"
+      ></div>
+      <span className="animate-pulse text-xl">Tunggu sebentar sedang di proses</span>
+    </div>
+  )
+}
