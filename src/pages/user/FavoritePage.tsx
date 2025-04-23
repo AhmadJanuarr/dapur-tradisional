d950f1051fd25d371cd3785f1891323169c229e5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 17:30:01 +0700

    feat: enhance FavoritePage with loading skeleton, error handling, and improved recipe rendering

--- src/pages/user/FavoritePage.tsx
+++ src/pages/user/FavoritePage.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useFetchDataRecipeFavorite } from "@/api/useFetchDataRecipeFavorite"
 import { BookMarksButton } from "@/components/Button/Bookmark"
+import { RecipeSkeleton } from "@/components/Skeleton/RecipeSkeleton"
 import { Button } from "@/components/ui/button"
 import { Card, CardDescription, CardTitle } from "@/components/ui/card"
 import { useAuth } from "@/context/AuthContext"
 import { useFavorite } from "@/hooks/useFavorite"
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { ChevronRight } from "lucide-react"
 import { useNavigate } from "react-router-dom"
+import { ErrorRecipe } from "../error/RecipeError"
+import { RecipeFavoriteNotFound } from "@/components/Recipe/RecipeFavoriteNotFound"
+import { RecipeData } from "@/types/recipe.types"
 
 export default function FavoritePage() {
   const { user } = useAuth()
@@ -19,35 +23,44 @@ export default function FavoritePage() {
     error,
   } = useQuery({
     queryKey: ["favorite"],
-    queryFn: useFetchDataRecipeFavorite().fetchData,
-    enabled: user?.name ? true : false,
+    enabled: user !== undefined || true,
     staleTime: 0,
     refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    placeholderData: (prevData) => prevData,
+    queryFn: useFetchDataRecipeFavorite().fetchData,
   })
 
-  console.log(recipes)
   const handleClickViewDetail = (title: string) => {
     navigate(`/resep/${title}`)
   }
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <RecipeSkeleton index={8} />
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>
+    return <ErrorRecipe />
   }
-
+  console.log(recipes)
   return (
     <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {recipes &&
-        recipes.map((item: any) => (
-          <Card className="relative flex w-full flex-col rounded-xl border-none bg-slate-100 shadow-none dark:bg-darkBackground">
+      {recipes ? (
+        recipes.map((item: RecipeData) => (
+          <Card
+            className="relative flex w-full flex-col rounded-xl border-none bg-slate-100 shadow-none dark:bg-darkBackground"
+            key={item.id}
+          >
             <div className="relative overflow-hidden">
               <div className="lg:h-52 2xl:h-72">
                 <img src={item.image} className="h-full w-full rounded-t-xl object-cover shadow-xl" alt={item.title} />
               </div>
-              <BookMarksButton onClickFavorite={() => handleClickFavorite} isFavorite={item.isFavorite} />
+              <div
+                onClick={() => handleClickFavorite(item.id)}
+                className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow"
+              >
+                <BookMarksButton isFavorite={item.isFavorite} />
+              </div>
             </div>
             <div className="flex h-32 flex-col justify-between gap-2 rounded-b-xl border border-slate-200 bg-white px-2 py-2 dark:bg-neutral-800 lg:h-[170px] lg:px-6 lg:py-4">
               <div className="flex flex-col gap-2 dark:text-black">
@@ -63,7 +76,10 @@ export default function FavoritePage() {
               </Button>
             </div>
           </Card>
-        ))}
+        ))
+      ) : (
+        <RecipeFavoriteNotFound />
+      )}
     </div>
   )
 }
