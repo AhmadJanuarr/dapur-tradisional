5a914010dc9c46e66ec4be97a3bf9bd2f3613c67
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 17:29:07 +0700

    feat: update RecipeCard component to improve bookmark button placement and styling

--- src/components/Card/RecipeCard.tsx
+++ src/components/Card/RecipeCard.tsx
@@ -5,7 +5,7 @@ import { BookMarksButton } from "../Button/Bookmark"
 import { RecipeCardProps } from "@/types/components.type"
 
 const RecipeImage = ({ img, title }: { img: string; title: string }) => (
-  <div className="lg:h-52 2xl:h-72">
+  <div className="h-36 lg:h-52 2xl:h-72">
     <img src={img} className="h-full w-full rounded-t-xl object-cover shadow-xl" alt={title} />
   </div>
 )
@@ -22,7 +22,12 @@ export const RecipeCard = ({
     <Card className="relative flex w-full flex-col rounded-xl border-none bg-slate-100 shadow-none dark:bg-darkBackground">
       <div className="relative overflow-hidden">
         <RecipeImage img={img} title={title} />
-        <BookMarksButton onClickFavorite={onClickFavorite} isFavorite={isFavorite} />
+        <div
+          onClick={onClickFavorite}
+          className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow"
+        >
+          <BookMarksButton isFavorite={isFavorite} />
+        </div>
       </div>
       <div className="flex h-32 flex-col justify-between gap-2 rounded-b-xl border border-slate-200 bg-white px-2 py-2 dark:bg-neutral-800 lg:h-[170px] lg:px-6 lg:py-4">
         <div className="flex flex-col gap-2 dark:text-black">
