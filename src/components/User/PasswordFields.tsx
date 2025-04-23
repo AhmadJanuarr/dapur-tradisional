69a5a13e9882d000eccfcf4111e7336513cf9376
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 17:29:26 +0700

    feat: improve layout and styling of PasswordFields component for better user experience

--- src/components/User/PasswordFields.tsx
+++ src/components/User/PasswordFields.tsx
@@ -17,8 +17,8 @@ export const PasswordFields = ({ password, setPassword }: PasswordFieldsProps) =
   const [showPassword, setShowPassword] = useState(false)
 
   return (
-    <div className="subheading flex justify-between">
-      <div className="flex gap-5">
+    <div className="subheading flex flex-col justify-between gap-2 lg:flex-row">
+      <div className="flex gap-2">
         <div className="flex flex-col gap-5">
           <label htmlFor="password" className="text-gray-800 dark:text-slate-200">
             Password sekarang
@@ -28,7 +28,7 @@ export const PasswordFields = ({ password, setPassword }: PasswordFieldsProps) =
             type={showPassword ? "text" : "password"}
             id="password"
             placeholder="*********"
-            className="h-12 w-full rounded-lg border-2 border-gray-300 px-4 outline-none focus:border-blue-500 dark:text-black dark:placeholder:text-black"
+            className="h-10 w-full rounded-lg border-2 border-gray-300 px-4 outline-none focus:border-blue-500 dark:text-black dark:placeholder:text-black lg:h-12"
           />
         </div>
         <div className="flex flex-col gap-5">
@@ -41,12 +41,12 @@ export const PasswordFields = ({ password, setPassword }: PasswordFieldsProps) =
             type={showPassword ? "text" : "password"}
             id="new-password"
             placeholder="*********"
-            className="h-12 w-full rounded-lg border-2 border-gray-300 px-4 outline-none focus:border-blue-500 dark:text-black dark:placeholder:text-black"
+            className="h-10 w-full rounded-lg border-2 border-gray-300 px-4 outline-none focus:border-blue-500 dark:text-black dark:placeholder:text-black lg:h-12"
           />
         </div>
       </div>
       <button
-        className="cursor-pointer font-semibold underline"
+        className="cursor-pointer pb-5 text-left font-semibold underline"
         type="button"
         onClick={() => setShowPassword((prev) => !prev)}
       >
