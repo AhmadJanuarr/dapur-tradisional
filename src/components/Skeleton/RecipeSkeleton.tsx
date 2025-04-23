cc5bd53d1d68362dfa415e47ce6c2a4e39a345fd
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-24 00:28:52 +0700

    feat: update layout in RecipeSkeleton for improved responsiveness

--- src/components/Skeleton/RecipeSkeleton.tsx
+++ src/components/Skeleton/RecipeSkeleton.tsx
@@ -2,11 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export const RecipeSkeleton = ({ index }: { index: number }) => {
   return (
-    <div className="flex w-full flex-col flex-wrap md:flex-row ">
+    <div className="grid w-full grid-cols-1 gap-3 lg:grid-cols-4">
       {Array(index)
         .fill(0)
         .map((_, i) => (
-          <div key={i} className="flex w-full flex-col space-y-3 p-5 lg:w-[25%]">
+          <div key={i} className="flex w-full flex-col space-y-3 p-5">
             <Skeleton className="h-[300px] w-full rounded-xl" />
             <div className="flex flex-col space-y-2">
               <Skeleton className="h-4 w-[250px]" />
