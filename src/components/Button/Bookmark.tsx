bc2316470f06c7dfad6ea0f1e05742fcc144be64
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-23 17:29:00 +0700

    feat: refactor BookMarksButton component for improved readability and structure

--- src/components/Button/Bookmark.tsx
+++ src/components/Button/Bookmark.tsx
@@ -2,15 +2,10 @@ import { BookmarksButtonProps } from "@/types/components.type"
 import { BookmarkIcon } from "@heroicons/react/24/outline"
 import { BookmarkIcon as BookmarkIconSolid } from "@heroicons/react/24/solid"
 
-export const BookMarksButton = ({ isFavorite, onClickFavorite }: BookmarksButtonProps) => (
-  <div
-    onClick={onClickFavorite}
-    className="absolute right-2 top-2 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white shadow"
-  >
-    {!isFavorite ? (
-      <BookmarkIcon className="h-6 w-6 text-[#f97316]" />
-    ) : (
-      <BookmarkIconSolid className="h-6 w-6 text-[#f97316]" />
-    )}
-  </div>
-)
+export const BookMarksButton = ({ isFavorite }: BookmarksButtonProps) => {
+  return !isFavorite ? (
+    <BookmarkIcon className="h-6 w-6 text-[#f97316]" />
+  ) : (
+    <BookmarkIconSolid className="h-6 w-6 text-[#f97316]" />
+  )
+}
