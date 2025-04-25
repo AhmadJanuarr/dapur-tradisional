e6240bf98615799e721aacf37d3546ca32a665d3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-26 01:21:41 +0700

    feat: add placeholder image prop and implement LazyLoadImage in HeroSectionRecipe for enhanced performance

--- src/Views/Recipes/HeroSection.tsx
+++ src/Views/Recipes/HeroSection.tsx
@@ -1,16 +1,18 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 type HeroSectionProps = {
   heading: string
   subheading: string
   image: string
+  placeholderImage: string
 }
 
-export const HeroSectionRecipe = ({ heading, subheading, image }: HeroSectionProps) => {
+export const HeroSectionRecipe = ({ heading, subheading, image, placeholderImage }: HeroSectionProps) => {
   return (
     <section className="mt-10 flex w-full lg:mt-28">
-      <div className="flex h-[500px] w-full flex-col-reverse gap-3 lg:flex-row ">
+      <div className="flex h-[500px] w-full flex-col-reverse lg:flex-row lg:gap-2">
         <div className="flex w-full flex-col justify-center rounded-xl bg-[#ffeddf] px-4 py-4 text-black md:py-10 lg:w-[40%] lg:gap-5 lg:px-6 lg:py-32">
           <h1 className="heading font-raleway leading-tight">{heading}</h1>
           <p className="subheading py-3 text-gray-800">{subheading}</p>
@@ -26,10 +28,18 @@ export const HeroSectionRecipe = ({ heading, subheading, image }: HeroSectionPro
             </form>
           </div>
         </div>
-        <div
-          className="z-10 flex h-[200px] w-full justify-center rounded-xl bg-center lg:h-full lg:w-[60%]"
-          style={{ backgroundImage: `url(${image})` }}
-        ></div>
+        <div className="relative w-full overflow-hidden rounded-xl lg:h-[500px] lg:w-[60%]">
+          <LazyLoadImage
+            placeholderSrc={placeholderImage}
+            src={image}
+            alt={image}
+            wrapperProps={{
+              style: { transitionDelay: "1s" },
+            }}
+            effect="blur"
+            className="h-full w-full object-cover"
+          />
+        </div>
       </div>
     </section>
   )
