ec03ef863cd6f033ffd42d4b0ecef4e4c9ed4079
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-26 00:53:25 +0700

    feat: enhance HoverOverlay component with loading state management

--- src/components/Hover/HoverOverlay.tsx
+++ src/components/Hover/HoverOverlay.tsx
@@ -1,17 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Upload, Loader2 } from "lucide-react"
 
-import { Upload } from "lucide-react"
-
-export const HoverOverlay = ({ onClick }: { onClick: () => void }) => {
+export const HoverOverlay = ({ isLoading, onClick }: { isLoading: boolean; onClick: () => void }) => {
   return (
     <div
-      className="absolute inset-0 flex cursor-pointer items-center justify-center rounded-full bg-black bg-opacity-50 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
-      onClick={onClick}
+      className={`absolute inset-0 flex cursor-pointer items-center justify-center rounded-full opacity-0 transition-opacity duration-200 ${isLoading ? "opacity-100" : "group-hover:opacity-100"}`}
+      onClick={!isLoading ? onClick : undefined}
     >
-      <span className="flex gap-2 text-white">
-        <Upload />
-        upload
-      </span>
+      {!isLoading ? (
+        <div className="flex h-full w-full items-center justify-center rounded-full bg-black bg-opacity-50">
+          <span className="flex gap-2 text-white">
+            <Upload />
+            upload
+          </span>
+        </div>
+      ) : (
+        <div className="flex h-full w-full items-center justify-center rounded-full bg-black bg-opacity-50">
+          <Loader2 className="w-16 animate-spin text-white" />
+        </div>
+      )}
     </div>
   )
 }
