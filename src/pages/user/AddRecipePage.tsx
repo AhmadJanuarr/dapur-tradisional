918928b73a8fc5a48fa5f9efb5519fdf9fb3c24d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-27 23:30:11 +0700

    feat: add AddRecipePage component with placeholder content

---
+++ src/pages/user/AddRecipePage.tsx
@@ -0,0 +1,7 @@
+export const AddRecipePage = () => {
+  return (
+    <div className="px-20">
+      <div className="heading flex h-96 items-center justify-center font-raleway">Coming soon</div>
+    </div>
+  )
+}
