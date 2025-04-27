ff6bef937876e2b737bed3c92d956a4f8e2c76e2
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-27 23:30:32 +0700

    feat: add NotificationPage component with placeholder content

---
+++ src/pages/user/NotificationPage.tsx
@@ -0,0 +1,7 @@
+export const NotificationPage = () => {
+  return (
+    <div className="px-20">
+      <div className="heading flex h-96 items-center justify-center font-raleway">Coming soon</div>
+    </div>
+  )
+}
