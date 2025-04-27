a9eadb3f92f53fa0c658efc0bd36b2a5511b091e
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-27 23:29:17 +0700

    feat: add RecipesPage component with sections for hero, categories, recipes, image switcher, and newsletter

---
+++ src/pages/public/RecipesPage.tsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react/jsx-runtime"
+import { CategorySection } from "@/Views/Recipes/CategorySection"
+import { HeroSectionRecipe } from "@/Views/Recipes/HeroSection"
+import { RecipesSection } from "@/Views/Recipes/RecipeSection"
+import { NewsLetter } from "@/Views/Home/NewsLetter"
+import { ImageSwitcherSection } from "@/Views/Recipes/ImageSwitcherSection"
+
+export default function RecipesPage() {
+  return (
+    <Fragment>
+      <HeroSectionRecipe
+        heading="Dapur tradisional - resep makanan khas daerah"
+        subheading="Temukan resep makanan khas daerah Indonesia yang lezat dan mudah dibuat"
+        image="/img/bg-recipe.png"
+        placeholderImage="/elements/element-placeholder-recipe.jpg"
+      />
+      <CategorySection />
+      <RecipesSection />
+      <ImageSwitcherSection />
+      <div className="mt-24 w-full lg:mt-40">
+        <NewsLetter />
+      </div>
+    </Fragment>
+  )
+}
