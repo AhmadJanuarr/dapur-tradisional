aea84ced4d75a1a285bb149465b9cc69cfe2903a
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-27 23:30:24 +0700

    fix: remove Notification component as it is no longer needed

--- src/pages/user/Notification.tsx
+++
@@ -1,7 +0,0 @@
-export const Notification = () => {
-  return (
-    <div className="px-20">
-      <div className="heading flex h-96 items-center justify-center font-raleway">Coming soon</div>
-    </div>
-  )
-}
