63841a37cc160329b64ac460e91cff9952e6b5c3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-27 23:28:03 +0700

    fix: adjust loading component styles for improved visibility and responsiveness

--- src/components/Loading/LoadingManageRecipes.tsx
+++ src/components/Loading/LoadingManageRecipes.tsx
@@ -1,6 +1,6 @@
 export const LoadingManageRecipes = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 bg-gray-100 dark:bg-gray-900 ">
+    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-100 dark:bg-[#0C0A09]">
       <div
         className="text-surface inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
         role="status"
