583c9d2467531cc7b6779a1907e87b3304ca3d7d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-29 12:54:50 +0700

    add useFavorite file

--- src/hooks/useFavorite.ts
+++ src/hooks/useFavorite.ts
@@ -1,4 +1,4 @@
-import { useAuth } from "@/context/AuthContext"
+import { useAuth } from "@/context/auth/useAuth"
 import { AxiosWithAuth } from "@/lib/AxiosWithAuth"
 import { RecipeData } from "@/types/recipe.types"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
