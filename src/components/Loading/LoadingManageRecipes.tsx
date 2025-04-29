e4dd1f3f1cfdfd721bbc6cd44686c49a5d9784af
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-29 12:51:43 +0700

    refactor : provide props to load management recipes

--- src/components/Loading/LoadingManageRecipes.tsx
+++ src/components/Loading/LoadingManageRecipes.tsx
@@ -1,11 +1,11 @@
-export const LoadingManageRecipes = () => {
+export const LoadingManageRecipes = ({ title }: { title: string }) => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-100 dark:bg-[#0C0A09]">
+    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-transparent">
       <div
         className="text-surface inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
         role="status"
       ></div>
-      <span className="animate-pulse text-xl">Tunggu sebentar sedang di proses</span>
+      <span className="animate-pulse text-center text-xl">{title}</span>
     </div>
   )
 }
