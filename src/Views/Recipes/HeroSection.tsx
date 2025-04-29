0cfaf5b8cdc073561f3a9af95c7883ab6e6d2ee8
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-29 13:02:03 +0700

    refactor code and add function handle recipe search

--- src/Views/Recipes/HeroSection.tsx
+++ src/Views/Recipes/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { useFeature } from "@/context/features/useFeature"
 import { useEffect, useRef, useState } from "react"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import { useNavigate } from "react-router-dom"
@@ -14,9 +14,27 @@ type HeroSectionProps = {
 
 export const HeroSectionRecipe = ({ heading, subheading, image, placeholderImage }: HeroSectionProps) => {
   const [isBoxOpen, setIsBoxOpen] = useState<boolean>(false)
-  const keyword = useRef<HTMLInputElement>(null)
+  const { keywordRef } = useFeature()
   const boxRef = useRef<HTMLDivElement>(null)
   const navigate = useNavigate()
+
+  const handleRecipeSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const KeywordValue = keywordRef.current?.value
+    const editKeywordValue = KeywordValue?.trim().replace(" ", "-")
+    console.log({ editKeywordValue })
+    if (!editKeywordValue) {
+      toast.warning("Masukkan kata kunci pencarian resep")
+    }
+    try {
+      navigate(`/resep/cari-resep?pencarian=${editKeywordValue}`)
+    } catch (error) {
+      console.log(error)
+      toast.error("Terjadi kesalahan pencarian resep")
+    } finally {
+      setIsBoxOpen(false)
+    }
+  }
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -28,53 +46,49 @@ export const HeroSectionRecipe = ({ heading, subheading, image, placeholderImage
     return () => document.removeEventListener("mousedown", handleClickOutside)
   }, [])
 
-  const handleRecipeSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    const valueKeyword = keyword.current?.value
-    console.log(valueKeyword)
-
-    e.preventDefault()
-    if (!valueKeyword) {
-      toast.warning("Masukkan kata kunci pencarian resep")
-    }
-    try {
-      navigate(`/resep/cari-resep?pencarian=${valueKeyword}`)
-    } catch (error) {
-      console.log(error)
-      toast.error("Terjadi kesalahan pencarian resep")
-    }
-  }
-
   return (
-    <section className="mt-10 flex w-full lg:mt-28">
-      <div className="flex h-[500px] w-full flex-col-reverse lg:flex-row lg:gap-2">
+    <section className="mt-24 flex w-full lg:mt-28">
+      <div className="flex h-[500px] w-full flex-col-reverse gap-2 lg:flex-row">
         <div className="flex w-full flex-col justify-end rounded-xl bg-[#ffeddf] px-4 py-4 text-black md:py-10 lg:w-[40%] lg:gap-5 lg:px-6">
-          <img src="/logo/logo-t.png" alt="logo website" className="mx-auto w-40" />
+          <img src="/logo/logo-t.png" alt="logo website" className="mx-auto w-28 lg:w-40" />
           <h1 className="heading font-raleway leading-tight">{heading}</h1>
           <p className="subheading py-3 text-gray-800">{subheading}</p>
           <div className="relative" ref={boxRef}>
-            <form className="flex w-full items-center justify-center gap-3" onSubmit={handleRecipeSearch}>
-              <Input
-                ref={keyword}
+            <form className="subheading flex w-full items-center gap-3" onSubmit={handleRecipeSearch}>
+              <input
                 type="search"
                 placeholder="Cari resep..."
-                className={`w-full rounded-md border-none text-black placeholder:text-[#656565] ${isBoxOpen ? "bg-white" : "bg-slate-100"}`}
                 aria-label="Search recipes"
+                className={`w-full rounded-md border-none p-2 text-black placeholder:text-[#656565] ${isBoxOpen ? "bg-white" : "bg-slate-100"}`}
+                ref={keywordRef}
                 onClick={() => setIsBoxOpen(true)}
               />
               <Button className="rounded-md" type="submit">
                 Pencarian
               </Button>
               {isBoxOpen && (
-                <div className="absolute left-0 right-0 top-0 z-20 mt-12 h-52 rounded-md border border-none bg-white p-4 shadow-lg">
+                <div className="absolute left-0 right-0 top-0 z-20 mt-12 rounded-md border bg-white p-4 shadow-xl">
                   <div className="flex flex-col">
-                    <div className="flex">
+                    <div className="flex justify-between">
                       <h1 className="font-semibold">Pencarian sebelumnya</h1>
-                      <span className="cursor-pointer text-slate-500"> . clear</span>
+                      <span className="cursor-pointer text-slate-500">clear</span>
                     </div>
                     <div className="py-2">
                       <Button variant="outline" className="borer-2 rounded-md bg-transparent">
                         Ayam penyet
                       </Button>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <h1 className="font-semibold">Filter</h1>
+                      <span className="cursor-pointer text-slate-500">clear</span>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      {["Makanan ringan", "Makanan berat", "Kue tradisional"].map((item) => (
+                        <label key={item} className="flex items-center gap-2">
+                          <input type="checkbox" id={item.toLowerCase().replace(/\s+/g, "-")} />
+                          {item}
+                        </label>
+                      ))}
                     </div>
                   </div>
                 </div>
