703240bab97da244e52b80f73ab6ec3f0689b46f
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-04-29 12:45:49 +0700

    refactor : change path useAuth

--- src/api/useFetchDataRecipeFavorite.ts
+++ src/api/useFetchDataRecipeFavorite.ts
@@ -1,4 +1,4 @@
-import { useAuth } from "@/context/AuthContext"
+import { useAuth } from "@/context/auth/useAuth"
 import { AxiosWithAuth } from "@/lib/AxiosWithAuth"
 import { useNavigate } from "react-router-dom"
 import { toast } from "sonner"
