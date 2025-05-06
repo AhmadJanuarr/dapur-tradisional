641e89e67240791e28a1624b700f15ff9e5d210c
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-06 21:33:53 +0700

    refactor code

--- src/Views/Contact/HeroSection.tsx
+++ src/Views/Contact/HeroSection.tsx
@@ -2,25 +2,31 @@ import { FormFieldContact } from "@/components/FormField/FormFieldContact"
 import { FaDiscord, FaInstagram, FaTelegram, FaWhatsapp } from "react-icons/fa6"
 
 const ContactInfo = () => {
+  const handleClickSosialMedia = (url: string) => {
+    if (url === "discord") window.open("https://discord.gg/J9UBsvBtH4", "_blank")
+    if (url === "instagram") window.open("https://www.instagram.com/madds.dev/", "_blank")
+    if (url === "telegram") window.open("https://t.me/ahmad_januar", "_blank")
+  }
+
   return (
     <div className="flex flex-col gap-5">
       <h5 className="text-[15px] lg:text-[18px]">Bicara dengan kami</h5>
       <p className="subheading text-gray-800 dark:text-slate-400">
         Butuh respon cepat? Langsung chat dengan kami melalui sosial media dibawah ini
       </p>
-      <div className="flex items-center gap-3">
+      <div className="flex cursor-pointer items-center gap-3" onClick={() => handleClickSosialMedia("discord")}>
         <FaDiscord className="size-5 text-[#F97316]" />
         <p className="underline">Discord</p>
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex cursor-pointer items-center gap-3" onClick={() => handleClickSosialMedia("instagram")}>
         <FaInstagram className="size-5 text-[#F97316]" />
         <p className="underline">Instagram</p>
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex cursor-pointer items-center gap-3" onClick={() => handleClickSosialMedia("telegram")}>
         <FaTelegram className="size-5 text-[#F97316]" />
         <p className="underline">Telegram</p>
       </div>
-      <h5 className="text-[15px] lg:text-[18px] ">Hubungi kami</h5>
+      <h5 className="text-[15px] lg:text-[18px]">Hubungi kami</h5>
       <p className="text-gray-800 dark:text-slate-400">Chat secara pribadi melalui whatsapp</p>
       <div className="flex items-center gap-3">
         <FaWhatsapp className="size-5 text-[#F97316]" />
