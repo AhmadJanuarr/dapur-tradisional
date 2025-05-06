89cca5f3062ca3297afc0a7b41db1b70d2be2872
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-06 21:28:50 +0700

    feat : new feature send mail

--- src/components/FormField/FormFieldContact.tsx
+++ src/components/FormField/FormFieldContact.tsx
@@ -1,22 +1,25 @@
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Textarea } from "../ui/textarea"
+import React, { useState } from "react"
+import axios from "axios"
+import { toast } from "sonner"
 
 type FieldProps = {
   label: string
+  name: string
   type?: string
   placeholder?: string
 }
 
-type NameFieldProps = {
-  fields: FieldProps[]
-}
-
-const FieldInput = ({ label, type, placeholder }: FieldProps) => {
+const FieldInput = ({ label, name, type = "text", placeholder }: FieldProps) => {
   return (
     <div className="flex flex-col gap-2">
-      <label>{label}</label>
+      <label htmlFor={name}>{label}</label>
       <Input
+        required
+        name={name}
+        id={name}
         type={type}
         placeholder={placeholder}
         className="px-6 py-6 text-gray-500 shadow-none dark:border-white dark:text-white dark:placeholder:text-white"
@@ -25,43 +28,55 @@ const FieldInput = ({ label, type, placeholder }: FieldProps) => {
   )
 }
 
-const NameFieldInput = ({ fields }: NameFieldProps) => {
-  return (
-    <div className="grid grid-cols-2 gap-5">
-      {fields.map((item, index) => (
-        <div key={index} className="flex flex-col gap-3">
-          <label>{item.label}</label>
-          <Input
-            placeholder={item.placeholder}
-            className="px-6 py-6 text-gray-500 shadow-none dark:border-white dark:text-white dark:placeholder:text-white"
-          />
-        </div>
-      ))}
-    </div>
-  )
-}
-
 export const FormFieldContact = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const onSubmitSendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const sendData = {
+      name: formData.get("name"),
+      subject: formData.get("subject"),
+      email: formData.get("email"),
+      message: formData.get("message"),
+    }
+    setIsLoading(true)
+    try {
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/email/send`, sendData)
+      toast.success("Pesan berhasil dikirim")
+      setIsLoading(false)
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        console.error(error.response.data.message)
+        toast.error(error.response.data.message)
+      } else {
+        console.error("An unexpected error occurred", error)
+      }
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitSendEmail}>
       <div className="subheading font-inter flex w-full flex-col gap-6">
-        <NameFieldInput
-          fields={[
-            { label: "Nama depan", placeholder: "John" },
-            { label: "Nama belakang", placeholder: "Doe" },
-          ]}
-        />
-        <FieldInput label="Email" type="email" placeholder="you@example.com" />
+        <FieldInput label="Nama" name="name" placeholder="Nama lengkap Anda" />
+        <FieldInput label="Subjek" name="subject" placeholder="Topik pesan Anda" />
+        <FieldInput label="Email" name="email" type="email" placeholder="you@example.com" />
         <div className="flex flex-col gap-3">
-          <label className="font-inter">Pesan</label>
+          <label htmlFor="message" className="font-inter">
+            Pesan
+          </label>
           <Textarea
+            required
+            name="message"
+            id="message"
             placeholder="Masukkan pesan Anda"
             rows={5}
             className="font-inter rounded-md px-6 py-5 text-gray-500 dark:border-white dark:text-white dark:placeholder:text-white"
           />
         </div>
-        <Button type="submit" variant="default" className="rounded-md py-5 font-semibold">
-          Kirim pesan
+        <Button type="submit" variant="default" className="rounded-md py-5 font-semibold" disabled={isLoading}>
+          {isLoading ? "Anda sedang mengirim" : "Kirim"}
         </Button>
       </div>
     </form>
